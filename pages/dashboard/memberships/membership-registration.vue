0ed4428eee6af0b6bc9dd26fb29d4510017514e7
Bikathi Martin <bikathimartin@outlook.com>
2024-03-11 10:28:48 +0300

    Build up UI for membership registration

--- pages/dashboard/memberships/membership-registration.vue
+++ pages/dashboard/memberships/membership-registration.vue
@@ -1,16 +1,41 @@
 <template>
 	<div class="py-10 bg-[#f6f9f2] responsive-view h-full">
-		<div class="bg-white w-full p-10">
-			<div class="flex items-center justify-between">
-				<div>
-					<span>Individual Registration</span>
-					<span>Bulk / Fleet Registration</span>
+		<div class="bg-white w-full p-10 px-2 lg:px-10">
+			<div
+				class="flex flex-col lg:flex-row space-y-3 lg:space-y-0 items-center justify-between">
+				<!-- TODO: fix issue with the border-b class to get rid of white space due to space-x-4 -->
+				<div
+					class="text-sm md:text-base lg:text-lg border-b-1 space-x-4 w- lg:w-fit">
+					<button
+						@click="() => (currentRegForm = 0)"
+						:class="[
+							'border-b-2 pb-1',
+							currentRegForm === 0
+								? 'border-b-blue-500'
+								: 'border-b-inherit',
+						]">
+						<span>Individual Registration</span>
+					</button>
+					<button
+						@click="() => (currentRegForm = 1)"
+						:class="[
+							'border-b-2 pb-1',
+							currentRegForm === 1
+								? 'border-b-blue-500'
+								: 'border-b-inherit',
+						]">
+						<span>Bulk / Fleet Registration</span>
+					</button>
 				</div>
 				<NuxtLink
-					:to="{ name: 'new-member' }"
+					:to="{ name: 'memberships-home' }"
 					class="bg-blue-600 text-white font-semibold text-sm p-4 rounded-md w-full md:w-fit text-center"
 					>VIEW ALL MEMBERS</NuxtLink
 				>
+			</div>
+			<div class="mt-16">
+				<IndividualMembershipReg v-if="currentRegForm === 0" />
+				<BulkMembershipReg v-else-if="currentRegForm === 1" />
 			</div>
 		</div>
 	</div>
@@ -21,4 +46,6 @@
 		name: "membership-registration",
 		layout: "in-app-layout",
 	});
+
+	const currentRegForm = ref(0);
 </script>
