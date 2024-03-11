34ba0837ac7b8a37a6548883626829c7845e1a69
Bikathi Martin <bikathimartin@outlook.com>
2024-03-11 12:27:37 +0300

    Reduce size of chat, notifications and user profile widgets

--- components/misc/TopnavAccentBoxes.vue
+++ components/misc/TopnavAccentBoxes.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="w-14 relative rounded-3xl h-14 bg-gray-300 flex justify-center items-center">
+		class="w-12 relative rounded-2xl h-12 bg-gray-300 flex justify-center items-center">
 		<slot name="iconSlot" />
 		<span
 			class="inline-flex items-center py-1.5 px-2 absolute -top-2 -right-3 rounded-full text-xs font-medium bg-pink-600 text-white"
