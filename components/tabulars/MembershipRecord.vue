963f5f4a34aa13089594965fc4e174a373e356a6
Bikathi Martin <bikathimartin@outlook.com>
2024-03-11 15:30:50 +0300

    Add page and table to show client membership details

---
+++ components/tabulars/MembershipRecord.vue
@@ -0,0 +1,70 @@
+<template>
+	<tr class="hover:shadow-lg">
+		<td class="px-6 py-4 whitespace-nowrap font-semibold text-pink-600">
+			KDF 345Y
+		</td>
+		<td class="px-6 py-4 whitespace-nowrap text-gray-600 font-semibold">
+			11/03/2024
+		</td>
+		<td class="px-6 py-4 whitespace-nowrap text-gray-600 font-semibold">
+			10/03/2025
+		</td>
+		<td
+			class="px-6 text-center py-4 whitespace-nowrap w-full text-gray-500 font-medium inline-flex flex-col">
+			<span class="text-start">Toyota</span>
+			<span class="text-start">Camri</span>
+		</td>
+		<td
+			class="px-6 py-4 whitespace-nowrap text-center font-semibold text-blue-500">
+			Individual
+		</td>
+		<td
+			class="px-6 py-4 whitespace-nowrap text-center font-semibold text-green-500">
+			Active
+		</td>
+		<td>
+			<div class="hs-dropdown relative inline-flex [--placement:left]">
+				<button
+					id="hs-dropdown-hover-event"
+					type="button"
+					class="hs-dropdown-toggle py-3 px-4">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1.5em"
+						height="2em"
+						viewBox="0 0 24 24">
+						<path
+							fill="none"
+							stroke="currentColor"
+							stroke-linejoin="round"
+							stroke-width="3.75"
+							d="M12 12h.01v.01H12zm0-7h.01v.01H12zm0 14h.01v.01H12z" />
+					</svg>
+				</button>
+
+				<div
+					class="hs-dropdown-menu border transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full font-semibold"
+					aria-labelledby="hs-dropdown-hover-event">
+					<button
+						class="flex items-center gap-x-3.5 p-3 rounded-t-lg w-full text-sm text-red-700 hover:bg-red-300 focus:outline-none">
+						Delete Membership
+					</button>
+					<!-- TODO: create page to edit membership -->
+					<NuxtLink
+						class="flex items-center gap-x-3.5 p-3 rounded-b-lg text-sm text-blue-700 hover:bg-blue-300 focus:outline-none"
+						:to="{
+							name: 'membership-details',
+							query: {
+								clientName: 'James Otieno',
+								numberOfVehicles: 5,
+								clientPhone: '+254113883976',
+								clientEmail: 'jamesotieno@gmail.com',
+							},
+						}"
+						>Edit Membership</NuxtLink
+					>
+				</div>
+			</div>
+		</td>
+	</tr>
+</template>
---
+++ pages/dashboard/memberships/membership-details.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="py-10 h-fit responsive-view">
+		<div
+			class="flex flex-col lg:flex-row space-x-0 lg:space-x-4 space-y-3 lg:space-y-0">
+			<div
+				class="w-full lg:w-1/2 md:flex-row rounded-md items-center justify-between py-3 px-3 md:px-6 border-2 border-gray-200 shadow-sm space-y-2 md:space-y-0">
+				<h1 class="text-2xl antialiased font-semibold">
+					{{ $route.query.clientName }}
+				</h1>
+				<div class="w-fit text-gray-500">
+					<h2>{{ $route.query.clientEmail }}</h2>
+					<h2>{{ $route.query.clientPhone }}</h2>
+				</div>
+			</div>
+			<div
+				class="w-full lg:w-1/2 md:flex-row rounded-md items-center justify-between py-3 px-3 md:px-6 border-2 border-gray-200 shadow-sm space-y-2 md:space-y-0">
+				<h1 class="text-2xl antialiased font-semibold">
+					Number of Vehicles
+				</h1>
+				<h2 class="text-xl text-gray-500">
+					{{ $route.query.numberOfVehicles }}
+				</h2>
+			</div>
+		</div>
+
+		<!-- start of data table -->
+		<div class="flex flex-col mt-5">
+			<div class="-m-1.5 overflow-x-auto">
+				<div class="p-1.5 min-w-full inline-block align-middle">
+					<div class="border rounded-sm shadow overflow-hidden">
+						<table class="min-w-full divide-y">
+							<thead>
+								<tr>
+									<th
+										scope="col"
+										class="px-6 py-3 text-start font-bold text-gray-500">
+										Vehicle Registration
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-start font-bold text-gray-500">
+										Start Date
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-start font-bold text-gray-500">
+										End Date
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-start font-bold text-gray-500">
+										Make & Model
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-center font-bold text-gray-500">
+										Membership Type
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-start font-bold text-gray-500">
+										Membership Status
+									</th>
+									<th
+										scope="col"
+										class="px-6 py-3 text-end" />
+								</tr>
+							</thead>
+							<tbody class="divide-y divide-gray-200">
+								<MembershipRecord v-for="a in 5" />
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end of data table -->
+	</div>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: "membership-details",
+		layout: "in-app-layout",
+	});
+</script>
