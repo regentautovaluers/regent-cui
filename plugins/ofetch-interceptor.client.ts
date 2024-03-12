f72ed1d25dce99e8cabfa581882507b379cedcd5
Bikathi Martin <bikathimartin@outlook.com>
2024-03-12 20:54:58 +0300

    Slim down logic for request interception to inject data

--- plugins/ofetch-interceptor.client.ts
+++ plugins/ofetch-interceptor.client.ts
@@ -1,46 +1,41 @@
 import { ofetch } from "ofetch";
 
 export default defineNuxtPlugin((_nuxtApp) => {
-    const authToken = useCookie('AUTH-TOKEN');
-	const csrfToken = useCookie('CSRF-TOKEN');
-    const allowedNonCSRFMethods: readonly String[] = ["GET", "HEAD", "TRACE", "OPTIONS"];
-    const { openToast } = useToast();
+	const authToken = useCookie("corp_auth_token");
+	const { openToast } = useToast();
 
-    globalThis.$fetch = ofetch.create({
-        async onRequest({ request, options }) {
-            let headers = {};
+	globalThis.$fetch = ofetch.create({
+		async onRequest({ options }) {
+			let headers = {};
+			headers["Authorization"] = `Bearer ${authToken.value}`;
 
-            if(!allowedNonCSRFMethods.includes(options.method)) {
-                headers['X-CSRF-TOKEN'] = csrfToken.value;
-                options.credentials = "include";
-            }
+			options.headers = { ...options.headers, ...headers };
+		},
 
-            if(!request.toString().includes("/auth")) {
-                headers['Authorization'] = `Bearer ${authToken.value}`;
-            }
+		/*
+		async onResponseError({ response }) {
+			const responseBody = response._data;
+			console.log(
+				"inside request interceptor: responseBody: ",
+				responseBody
+			);
+			switch (responseBody.status) {
+				case 401: {
+					openToast(responseBody.message, "danger");
+					break;
+				}
 
-            options.headers = {...options.headers, ...headers};
-        },
+				case 500: {
+					openToast(responseBody.message, "warning");
+					break;
+				}
 
-        async onResponseError({ response }) {
-            const responseBody = response._data;
-            console.log("inside request interceptor: responseBody: ", responseBody);
-            switch(responseBody.status) {
-                case 401: {
-                    openToast(responseBody.message, 'danger');
-                    break;
-                }
-
-                case 500: {
-                    openToast(responseBody.message, 'warning');
-                    break
-                }
-
-                case 404: {
-                    openToast(responseBody.message, 'warning');
-                    break;
-                }
-            }
-        }
-    });
+				case 404: {
+					openToast(responseBody.message, "warning");
+					break;
+				}
+			}
+		},
+        */
+	});
 });
