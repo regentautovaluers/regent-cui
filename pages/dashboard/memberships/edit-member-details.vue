b38f3d875023ad4c451a1ab9c7df4418a2cfce23
Bikathi Martin <bikathimartin@outlook.com>
2024-03-12 08:40:27 +0300

    Rename page for better communication of purpose

---
+++ pages/dashboard/memberships/edit-member-details.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="py-10 bg-[#f6f9f2] responsive-view h-full">
+		<div class="bg-white w-full p-10 px-2 lg:px-10">
+			<h1 class="my-4 text-2xl antialiased font-semibold">
+				Editing Details for {{ $route.query.memberName }}
+			</h1>
+			<EditMemberDetails />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: "edit-member-details",
+		layout: "in-app-layout",
+	});
+</script>
