a28952489ca4ef5b6db7436af5d5fb98aac6825c
Bikathi Martin <bikathimartin@outlook.com>
2024-03-12 20:59:51 +0300

    Configure extra parameters for toast widget

--- composables/useToast.ts
+++ composables/useToast.ts
@@ -1,18 +1,28 @@
-import { createToast, type ToastType } from 'mosha-vue-toastify';
-import 'mosha-vue-toastify/dist/style.css';
+import { createToast, type ToastType } from "mosha-vue-toastify";
+import "mosha-vue-toastify/dist/style.css";
 
-export default function() {
-    // types- default, info, warning, success, danger
-    function openToast(message: string, type: string, description?: string,): void {
-        createToast({
-            title: message,
-            description: description
-        },
-        {
-            timeout: 2000,
-            type: type as ToastType
-        })
-    }
+export default function () {
+	// types- default, info, warning, success, danger
+	function openToast(
+		message: string,
+		type: string,
+		description?: string
+	): void {
+		createToast(
+			{
+				title: message,
+				description: description,
+			},
+			{
+				timeout: 3000,
+				type: type as ToastType,
+				showCloseButton: false,
+				transition: "bounce",
+				hideProgressBar: true,
+                showIcon: true,
+			}
+		);
+	}
 
-    return { openToast }
+	return { openToast };
 }
