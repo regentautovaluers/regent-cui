82d67f8be518f18b15f40bfbcb93eafab8d12430
Bikathi Martin <bikathimartin@outlook.com>
2024-03-12 20:52:47 +0300

    Create store to manage logged in user entity

---
+++ stores/usePrincipal.ts
@@ -0,0 +1,16 @@
+import { useStorage } from "@vueuse/core";
+
+export const usePrincipal = defineStore("PrincipalStore", () => {
+	const principal = useStorage("principal", {});
+
+	const getDetails = computed(() => principal.value);
+
+	async function setDetails(userDetails: Object): Promise<void> {
+		principal.value = userDetails;
+	}
+
+	return {
+		getDetails,
+		setDetails,
+	};
+});
