6ff894c8cd78ef40f172018130082f563bba8d10
Bikathi Martin <bikathimartin@outlook.com>
2024-03-14 13:24:16 +0300

    Create type to represent details for creating bulk membership

--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,32 @@
 export type ApplicationChildRoute = {
-    id: number;
-    displayName: string;
-    routeName: string;
+	id: number;
+	displayName: string;
+	routeName: string;
 };
 
 export type ApplicationRoute = {
-    id: number;
-    displayName: string;
-    icon: string;
-    routeName: string;
-    children?: ApplicationChildRoute[];
+	id: number;
+	displayName: string;
+	icon: string;
+	routeName: string;
+	children?: ApplicationChildRoute[];
+};
+
+export type bulkProcessedType = {
+	full_name: string;
+	phone_number: string;
+	userEmail: string;
+	corporateId: number;
+	membershipTypeId: number;
+	start_date: string;
+	end_date: string;
+	make: string;
+	model: string;
+	color: string;
+	payment_status: string;
+	membership_status: string;
+	registration: string;
+	recordedBy: string;
+	category: string;
+	fleetId: number;
 };
