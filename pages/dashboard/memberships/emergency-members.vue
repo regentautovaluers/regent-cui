c27cf9d75de47607e551ca9111795b3fb9ada6ee
Bikathi Martin <bikathimartin@outlook.com>
2024-03-23 21:45:08 +0300

    Get rid of filter controls

--- pages/dashboard/memberships/emergency-members.vue
+++ pages/dashboard/memberships/emergency-members.vue
@@ -19,63 +19,6 @@
 						alt="Search Icon" />
 				</div>
 			</div>
-			<!-- filter controls -->
-			<div class="flex flex-nowrap items-center space-x-3">
-				<!-- filter by Membership type -->
-				<div
-					class="hs-dropdown relative inline-flex [--placement:bottom-right]">
-					<button
-						id="hs-dropdown-default"
-						type="button"
-						class="hs-dropdown-toggle py-3 px-4 inline-flex h-12 items-center gap-x-2 font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-						Membership Category
-						<svg
-							class="hs-dropdown-open:rotate-180 size-4"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round">
-							<path d="m6 9 6 6 6-6" />
-						</svg>
-					</button>
-					<div
-						class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full space-y-2"
-						aria-labelledby="hs-dropdown-default">
-						<div
-							class="flex hover:bg-gray-200 p-2 rounded-lg items-center">
-							<input
-								type="radio"
-								name="hs-default-radio"
-								class="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-								id="hs-default-radio" />
-							<label
-								for="hs-default-radio"
-								class="text-gray-500 ms-2 dark:text-gray-400"
-								>Corporate Registration</label
-							>
-						</div>
-						<div
-							class="flex hover:bg-gray-200 p-2 rounded-lg items-center">
-							<input
-								type="radio"
-								name="hs-default-radio"
-								class="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-								id="hs-checked-radio"
-								checked />
-							<label
-								for="hs-checked-radio"
-								class="text-gray-500 ms-2 dark:text-gray-400"
-								>Individual Registration</label
-							>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 		<!-- start of data table -->
 		<div class="flex flex-col">
@@ -113,12 +56,16 @@
 							<tbody class="divide-y divide-gray-200">
 								<Suspense>
 									<template #default>
-										<MembersRecord
+										<FilteredMembersRecord
+											:current-page="currentPage"
 											default-filter-flag="Emergency Evacuation"
-											@total-number="
-												(totalMembers: number) =>
-													(totalNumber = totalMembers)
-											" />
+											@provide-statistics="
+														(totalMembers: number , totPages: number) =>
+															{
+																totalNumber = totalMembers; 
+																totalPages = totPages;
+															}
+													" />
 									</template>
 
 									<template #fallback>
@@ -134,8 +81,37 @@
 			</div>
 		</div>
 		<!-- end of data table -->
-		<div class="mt-2 w-full rounded-sm flex justify-between py-2">
-			<span>Showing from {{ totalNumber }} possible members.</span>
+		<div
+			class="mt-2 w-full rounded-sm flex justify-between items-center py-2">
+			<span
+				>Showing {{ currentPage + 1 }} of {{ totalPages }} pages.</span
+			>
+			<div class="space-x-1">
+				<button
+					v-for="page in currentPage + 1"
+					:key="page"
+					@click="() => (currentPage = 0)"
+					type="button"
+					class="p-2 size-10 text-center text-sm font-semibold rounded-md border bg-blue-600 text-white hover:bg-blue-700">
+					{{ page }}
+				</button>
+				<button
+					v-if="totalPages > 1 && currentPage + 1 < totalPages"
+					@click="
+						() => {
+							if (currentPage + 1 < totalPages) currentPage++;
+						}
+					"
+					type="button"
+					class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-md border border-transparent bg-blue-600 text-white hover:bg-blue-700">
+					<span v-if="!fetchingMoreData">Next Page</span>
+					<div
+						v-if="fetchingMoreData"
+						class="animate-spin inline-block size-5 border-[3px] border-white border-current border-t-transparent text-gray-800 rounded-full"
+						role="status"
+						aria-label="loading" />
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -147,6 +123,10 @@
 	});
 
 	const totalNumber: Ref<number> = ref(0);
+	const currentPage: Ref<number> = ref(0);
+	const totalPages: Ref<number> = ref(0);
+	const { getDetails } = usePrincipal();
 	const searchFilterTerm: Ref<string> = ref("");
 	const searchMembershipCategory: Ref<string | null> = ref(null);
+	const fetchingMoreData: Ref<boolean> = ref(false);
 </script>
