57801408944c64937478c58157ef511c0ff01666
Bikathi Martin <bikathimartin@outlook.com>
2024-03-24 08:16:23 +0300

    Add the handwave icon to the greetings line

--- pages/dashboard/memberships/index.vue
+++ pages/dashboard/memberships/index.vue
@@ -9,9 +9,14 @@
 					src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
 					alt="User Image" />
 				<div class="flex flex-col">
-					<span class="text-blue-600 font-semibold"
-						>Hi, {{ getDetails.username }}</span
-					>
+					<span
+						class="text-blue-600 font-semibold inline-flex items-center"
+						>Hi, {{ getDetails.username }}
+						<img
+							src="/icons/misc/hand-wave.svg"
+							alt="Hand Wave Icon"
+							class="ml-1"
+					/></span>
 					<span>You Have {{ totalNumber }} Registered Members</span>
 				</div>
 			</div>
