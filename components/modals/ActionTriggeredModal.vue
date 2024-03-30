4c83971e5624cdf4c7b30314825f65df59937976
Bikathi Martin <bikathimartin@outlook.com>
2024-03-30 10:14:11 +0300

    Improve UI of modal and increase size of focus box

--- components/modals/ActionTriggeredModal.vue
+++ components/modals/ActionTriggeredModal.vue
@@ -8,9 +8,9 @@
 
 	<div
 		id="hs-basic-modal"
-		class="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
+		class="hs-overlay [--overlay-backdrop:static] hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
 		<div
-			class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+			class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
 			<div
 				class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
 				<div
