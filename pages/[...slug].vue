76894b85f3327810dbdc77c4bd7cf9e3a5836a49
Bikathi Martin <bikathimartin@outlook.com>
2024-04-12 15:10:02 +0300

    Create basic catch-all route

---
+++ pages/[...slug].vue
@@ -0,0 +1,15 @@
+<template>
+	<main class="w-screen h-screen flex flex-col justify-center items-center">
+		<img
+			src="/icons/misc/page-not-found.svg"
+			alt="Not Found Icon"
+			class="size-64" />
+
+		<h1 class="text-3xl font-bold mt-2">Page Not Found.</h1>
+		<NuxtLink
+			:to="{ name: 'authentication-page' }"
+			class="text-blue-500 underline underline-offset-2 mt-2"
+			>Take Me Home</NuxtLink
+		>
+	</main>
+</template>
