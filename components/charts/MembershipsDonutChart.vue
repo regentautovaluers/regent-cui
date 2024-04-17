924a95a8e69af3906127be8f710ef11fb08606f8
Bikathi Martin <bikathimartin@outlook.com>
2024-04-17 16:30:47 +0300

    Create code to render chart

--- components/charts/MembershipsDonutChart.vue
+++ components/charts/MembershipsDonutChart.vue
@@ -1,16 +1,50 @@
 <template>
-	<h1>chart should be here</h1>
+	<Doughnut
+		:data="chartData"
+		:options="chartOptions" />
 </template>
 
 <script setup lang="ts">
+	import { Doughnut } from "vue-chartjs";
 	const { getDetails } = usePrincipal();
 	const runtimeConfig = useRuntimeConfig();
-	const totalActive: Ref<number | null> = ref(null);
-	const totalInactive: Ref<number | null> = ref(null);
-	const totalPaid: Ref<number | null> = ref(null);
-	const totalUnpaid: Ref<number | null> = ref(null);
-	const totalMemberships: Ref<number | null> = ref(null);
+	const totalActive: Ref<number> = ref(0);
+	const totalInactive: Ref<number> = ref(0);
+	const totalPaid: Ref<number> = ref(0);
+	const totalUnpaid: Ref<number> = ref(0);
+	const totalMemberships: Ref<number> = ref(0);
+	const collectiveData = computed(() => {
+		return [
+			totalActive.value,
+			totalInactive.value,
+			totalPaid.value,
+			totalUnpaid.value,
+		];
+	});
 
+	const chartData = computed(() => ({
+		labels: ["Paid", "Unpaid", "Active", "Inactive"],
+		datasets: [
+			{
+				backgroundColor: ["#f87979", "#7d669e", "#53bbb4", "#f39c12"],
+				data: [
+					totalPaid.value || 0,
+					totalUnpaid.value || 0,
+					totalActive.value || 0,
+					totalInactive.value || 0,
+				],
+				spacing: 5,
+			},
+		],
+	}));
+	const chartOptions = ref({
+		responsive: true,
+		maintainAspectRatio: false,
+		cutoutPercentage: 70,
+		legend: {
+			display: false,
+		},
+	});
 	try {
 		await $fetch(
 			`${runtimeConfig.public.DEV_TIME_HOST}/api/v1/corp/reports/donut-graph-data`,
