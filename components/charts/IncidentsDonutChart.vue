78750840c72207c1769b61d6cdf7355eb32a48cc
Bikathi Martin <bikathimartin@outlook.com>
2024-04-18 14:52:33 +0300

    Make chart functionable and usable

--- components/charts/IncidentsDonutChart.vue
+++ components/charts/IncidentsDonutChart.vue
@@ -1,5 +1,45 @@
 <template>
-	<h1>Welcome to incidents</h1>
+	<Doughnut
+		:data="chartData"
+		:options="chartOptions" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { Doughnut } from "vue-chartjs";
+
+	const componentProps = defineProps<{
+		totalFuelDelivery: number;
+		totalJumpstarting: number;
+		totalTowing: number;
+		totalTyreChange: number;
+		totalCollective: number;
+	}>();
+	const countFuelDelivery = computed(() => componentProps.totalFuelDelivery);
+	const countJumpstarting = computed(() => componentProps.totalJumpstarting);
+	const countTowing = computed(() => componentProps.totalTowing);
+	const countTyreChange = computed(() => componentProps.totalTyreChange);
+	const countTotalIncidents = computed(() => componentProps.totalCollective);
+	const chartData = computed(() => ({
+		labels: ["Fuel Delivery", "Jumpstarting", "Towing", "Tyre Change"],
+		datasets: [
+			{
+				backgroundColor: ["#f87979", "#7d669e", "#53bbb4", "#f39c12"],
+				data: [
+					countFuelDelivery.value || 0,
+					countJumpstarting.value || 0,
+					countTowing.value || 0,
+					countTyreChange.value || 0,
+				],
+				spacing: 5,
+			},
+		],
+	}));
+	const chartOptions = ref({
+		responsive: true,
+		maintainAspectRatio: false,
+		cutoutPercentage: 70,
+		legend: {
+			display: true,
+		},
+	});
+</script>
