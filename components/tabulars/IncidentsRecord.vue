9762890984db5b943efb5677942fd9c98f970270
Bikathi Martin <bikathimartin@outlook.com>
2024-04-18 14:53:42 +0300

    Update dropdown UI and options

--- components/tabulars/IncidentsRecord.vue
+++ components/tabulars/IncidentsRecord.vue
@@ -18,54 +18,36 @@
 			{{ componentProps.serviceType }}
 		</td>
 		<td
-			class="px-6 py-4 whitespace-nowrap text-center font-semibold text-pink-500">
+			class="px-6 py-4 whitespace-nowrap text-ellipsis overflow-hidden text-end font-semibold text-pink-500">
 			{{ componentProps.location }}
 		</td>
 		<td>
-			<div class="hs-dropdown relative inline-flex">
+			<div class="hs-dropdown relative inline-flex [--placement:left]">
 				<button
 					id="hs-dropdown-default"
 					type="button"
-					class="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-					Actions
+					class="hs-dropdown-toggle">
 					<svg
-						class="hs-dropdown-open:rotate-180 size-4"
 						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<path d="m6 9 6 6 6-6" />
+						width="1.8em"
+						height="1.8em"
+						viewBox="0 0 24 24">
+						<path
+							fill="currentColor"
+							fill-rule="evenodd"
+							d="M10.5 5A1.5 1.5 0 0 1 12 3.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5zm0 7a1.5 1.5 0 0 1 1.5-1.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5zm1.5 5.5a1.5 1.5 0 0 0-1.5 1.5v.01a1.5 1.5 0 0 0 1.5 1.5h.01a1.5 1.5 0 0 0 1.5-1.5V19a1.5 1.5 0 0 0-1.5-1.5z"
+							clip-rule="evenodd" />
 					</svg>
 				</button>
 
 				<div
-					class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+					class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full border"
 					aria-labelledby="hs-dropdown-default">
-					<a
-						class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-						href="#">
-						Newsletter
-					</a>
-					<a
-						class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-						href="#">
-						Purchases
-					</a>
-					<a
-						class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-						href="#">
-						Downloads
-					</a>
-					<a
-						class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-						href="#">
-						Team Account
-					</a>
+					<NuxtLink
+						:to="{ name: 'ava-incident-report' }"
+						class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+						View Report
+					</NuxtLink>
 				</div>
 			</div>
 		</td>
