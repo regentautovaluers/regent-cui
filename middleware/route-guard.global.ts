54e9a480255d21b17ec77bc2e2dce50dcd6dcdc3
bikathi <bikathimartin@outlook.com>
2024-04-30 15:35:22 +0300

    Add various route-guard functionalities

--- middleware/route-guard.global.ts
+++ middleware/route-guard.global.ts
@@ -1,5 +1,48 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-	// if the user is trying to navigate from dashboard to a route outside the dashboard
-	// and they are already logged in, disable the navigation
-	console.log("route middleware running");
+	const router = useRouter();
+	const { assertSessionExists } = useSessionContext();
+	const { assertPrincipalExists } = usePrincipal();
+	const { openToast } = useToast();
+	const unprotectedRouteNames: string[] = [
+		"forgot-password",
+		"auth-error",
+		"authentication-page",
+	];
+
+	// when the user loads a page by manually typing the page url, the to.path/to.name and from.path/from.name are the same value.
+	// in such a case, if they have a session(via remember me) and the principal still exists, then allow navigation
+	// else take them to the login
+	if (
+		to.name === from.name &&
+		to.name !== "authentication-page" &&
+		!unprotectedRouteNames.includes(to.name as string)
+	) {
+		if (assertSessionExists() && assertPrincipalExists()) {
+			return;
+		} else {
+			openToast("Untrusted session. Please login!", "danger");
+			router.replace({
+				name: "authentication-page",
+			});
+
+			// if (typeof window !== "undefined") location.reload();
+		}
+	}
+
+	// when the user starts the app and they already had a session(via remember me) and there exists the principal,
+	// re-navigate them to the dashboard-home page
+	if (to.name === from.name && to.name === "authentication-page") {
+		if (assertSessionExists() && assertPrincipalExists()) {
+			openToast(
+				"Already Logged In. Redirecting to Dashboard Home!",
+				"success"
+			);
+
+			router.replace({
+				name: "dashboard-home",
+			});
+		} else {
+			return;
+		}
+	}
 });
