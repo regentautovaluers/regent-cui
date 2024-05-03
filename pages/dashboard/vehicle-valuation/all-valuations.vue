f5fa15b27df0bd204df767ccbb61e704a8fc833b
bikathi <bikathimartin@outlook.com>
2024-05-03 11:36:16 +0300

    Add function to search for valuations and reduce filters

--- pages/dashboard/vehicle-valuation/all-valuations.vue
+++ pages/dashboard/vehicle-valuation/all-valuations.vue
@@ -25,101 +25,40 @@
 					<span>Pending Valuations</span>
 				</button>
 			</div>
-
-			<!-- search box -->
-			<div class="relative flex-grow mr-10 max-w-[35%]">
-				<input
-					type="text"
-					class="peer py-3 h-12 px-4 bg-gray-200 border-transparent rounded-2xl focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none min-w-full"
-					placeholder="Search Registration Number Here"
-					v-model="searchFilterTerm" />
-				<button
-					class="absolute inset-y-0 end-0 flex justify-center rounded-2xl w-14 bg-blue-500 pointer-events-none p-2 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-					<img
-						src="/images/topnav/search-icon.svg"
-						alt="Search Icon" />
-				</button>
-			</div>
-			<div>
-				<!-- filter by Membership type -->
-				<div
-					class="hs-dropdown relative inline-flex [--placement:bottom-right]">
+			<div class="flex-grow flex justify-end items-center">
+				<!-- search box -->
+				<div class="relative flex-grow max-w-[40%]">
+					<input
+						type="text"
+						class="peer py-3 h-12 px-4 bg-gray-200 border-transparent rounded-2xl focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none min-w-full"
+						placeholder="Search Registration Number Here"
+						v-model="searchFilterTerm" />
 					<button
-						id="hs-dropdown-default"
-						type="button"
-						class="hs-dropdown-toggle py-3 px-4 inline-flex h-12 items-center gap-x-2 font-medium rounded-2xl border border-gray-200 text-gray-800 shadow-sm"
-						:class="
-							!searchByValuationDate
-								? 'bg-white text-black'
-								: 'bg-blue-600 text-white'
-						">
-						Membership Category
-						<svg
-							class="hs-dropdown-open:rotate-180 size-4"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round">
-							<path d="m6 9 6 6 6-6" />
-						</svg>
+						class="absolute inset-y-0 end-0 flex justify-center rounded-2xl w-14 bg-blue-500 pointer-events-none p-2 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
+						<img
+							src="/images/topnav/search-icon.svg"
+							alt="Search Icon" />
 					</button>
-					<div
-						class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 border after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full space-y-2 z-20"
-						aria-labelledby="hs-dropdown-default">
-						<div
-							v-for="(option, index) in [
-								{
-									text: 'Corporate Members',
-									id: 'corp-cat',
-									value: 'corporate',
-								},
-								{
-									text: 'Individual Members',
-									id: 'indiv-cat',
-									value: 'individual',
-								},
-							]"
-							:key="index"
-							class="flex hover:bg-gray-200 p-2 rounded-lg items-center">
-							<!-- prettier-ignore -->
-							<input
-											:id="option.id"
-											type="radio"
-											name="membership-category"
-											:value="option.value"
-											class="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-											v-model="searchByValuationDate" />
-							<label
-								for="corp-cat"
-								class="text-gray-500 ms-2 dark:text-gray-400"
-								>{{ option.text }}</label
-							>
-						</div>
-					</div>
 				</div>
-
-				<!-- button to disable filters -->
-				<button
-					v-if="
-						searchFilterTerm !== '' || searchByValuationDate !== ''
-					"
-					@click="
-						() => {
-							searchFilterTerm = '';
-							searchByValuationDate = '';
-						}
-					"
-					title="Clear Filters"
-					class="bg-gray-300 text-center p-2 rounded-full size-10 ml-2">
-					<Icon
-						name="material-symbols:close"
-						class="text-lg" />
-				</button>
+				<div>
+					<!-- button to disable filters -->
+					<button
+						v-if="
+							searchFilterTerm !== '' ||
+							searchByValuationDate !== ''
+						"
+						@click="
+							() => {
+								searchFilterTerm = '';
+							}
+						"
+						title="Clear Filters"
+						class="bg-gray-300 text-center p-2 rounded-full size-10 ml-2">
+						<Icon
+							name="material-symbols:close"
+							class="text-lg" />
+					</button>
+				</div>
 			</div>
 		</div>
 		<div
@@ -359,6 +298,7 @@
 	const searchByValuationDate: Ref<string> = ref("");
 	const recentValuations: Ref<any[]> = ref([]);
 	const pendingValuations: Ref<any[]> = ref([]);
+	const valuationsFromSearch: Ref<any[]> = ref([]);
 	const fetchCompleteErrorOrEmpty: Ref<boolean> = ref(false);
 	const fetchPendingErrorOrEmpty: Ref<boolean> = ref(false);
 	const currentPage: Ref<number> = ref(0);
@@ -392,20 +332,16 @@
 	}
 
 	function performValuationListsProcessing(sourceList: any[]): any[] {
-		// Filter the aggData array based on the searchFilterTerm and searchServiceType
+		// Filter the aggData array based on the searchFilterTerm
 		let filteredData = sourceList.filter((item) => {
 			// Check if the searchFilterTerm matches any of the specified fields
 			const termMatch = item.vehicleRegNumber.includes(
 				searchFilterTerm.value
 			);
 
-			// Check if the searchServiceType matches the service field
-			const serviceTypeMatch = item.service === searchServiceType.value;
-
 			// Return true if both filters match or if both filters are disabled (empty strings)
 			return (
 				termMatch ||
-				serviceTypeMatch ||
 				(searchFilterTerm.value === "" &&
 					searchServiceType.value === "")
 			);
@@ -478,6 +414,35 @@
 						"Successfully loaded your pending valuations",
 						"success"
 					);
+				},
+			});
+		} catch (error) {
+			console.log("An error occured: ", error);
+			openToast("Failed to load your members. Reload page!", "danger");
+		}
+	}
+
+	async function searchValuations() {
+		try {
+			await $fetch("/ava/api/assessment/search_assessment", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				query: {
+					uname: runtimeConfig.app.VALUATION_BASE_UNAME,
+					pwd: runtimeConfig.app.VALUATION_BASE_PASS,
+					corp: getDetails.company,
+					reg_no: searchFilterTerm.value,
+				},
+				async onResponse({ response }) {
+					if (response.status !== 200) {
+						throw new Error("Failed to retrieve incidents!");
+					}
+
+					valuationsFromSearch.value = JSON.parse(response._data);
+					fetchPendingErrorOrEmpty.value = false;
+					openToast("Search valuations successfull", "success");
 				},
 			});
 		} catch (error) {
