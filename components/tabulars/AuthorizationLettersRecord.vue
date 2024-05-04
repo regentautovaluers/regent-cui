74d612ecdb5e9ef1c4848caabd3bddfef3058c4a
bikathi <bikathimartin@outlook.com>
2024-05-04 10:05:35 +0300

    Create component to render rows in authorization letters list

---
+++ components/tabulars/AuthorizationLettersRecord.vue
@@ -0,0 +1,115 @@
+<template>
+	<tr class="hover:shadow-lg">
+		<td class="px-6 py-6 whitespace-nowrap font-semibold text-pink-600">
+			{{ componentProps.broker }}
+		</td>
+		<td class="px-6 py-6 whitespace-nowrap text-gray-600 font-semibold">
+			{{ componentProps.customer }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-start font-semibold text-blue-500">
+			{{ componentProps.policyNo }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-center font-semibold text-green-500">
+			{{ componentProps.registrationNo }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-end font-semibold text-gray-600">
+			{{ componentProps.authorizedBy }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-center font-semibold text-pink-600">
+			{{ componentProps.date }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-center font-semibold text-gray-600">
+			{{ componentProps.authRefNo }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-center font-semibold text-pink-600">
+			{{ componentProps.phoneNo }}
+		</td>
+		<td
+			class="px-6 py-6 whitespace-nowrap text-start font-semibold text-gray-600">
+			{{ componentProps.regentBranchEmail }}
+		</td>
+		<!-- <td>
+			<div
+				class="relative inline-block text-left"
+				ref="dropdownContainer">
+				<button @click="toggleDropdown">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1.8em"
+						height="1.8em"
+						viewBox="0 0 24 24">
+						<path
+							fill="currentColor"
+							fill-rule="evenodd"
+							d="M10.5 5A1.5 1.5 0 0 1 12 3.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5zm0 7a1.5 1.5 0 0 1 1.5-1.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5zm1.5 5.5a1.5 1.5 0 0 0-1.5 1.5v.01a1.5 1.5 0 0 0 1.5 1.5h.01a1.5 1.5 0 0 0 1.5-1.5V19a1.5 1.5 0 0 0-1.5-1.5z"
+							clip-rule="evenodd" />
+					</svg>
+				</button>
+				<div
+					v-if="isDropdownVisible"
+					@click.stop
+					class="custom-dropdown -top-[80%]">
+					<div
+						class="py-1"
+						role="menu"
+						aria-orientation="vertical"
+						aria-labelledby="options-menu">
+						<button
+							type="button"
+							data-hs-overlay="#hs-medium-modal"
+							class="w-full flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+							More Details
+						</button>
+					</div>
+				</div>
+			</div>
+		</td> -->
+	</tr>
+</template>
+
+<script setup lang="ts">
+	const componentProps = defineProps<{
+		broker: string;
+		customer: string;
+		policyNo: string;
+		registrationNo: string;
+		authorizedBy: string;
+		date: string;
+		authRefNo: string;
+		phoneNo: string;
+		regentBranchEmail: string;
+	}>();
+
+	const isDropdownVisible = ref(false);
+	const dropdownContainer = ref(null);
+
+	function toggleDropdown() {
+		isDropdownVisible.value = !isDropdownVisible.value;
+	}
+
+	function closeDropdown() {
+		isDropdownVisible.value = false;
+	}
+
+	function handleClickOutside(event: MouseEvent) {
+		if (!event.target) return;
+		// Check if the click is outside the dropdown container
+		if (!dropdownContainer.value.contains(event.target as Node)) {
+			closeDropdown();
+		}
+	}
+
+	onMounted(() => {
+		document.addEventListener("click", handleClickOutside);
+	});
+
+	onBeforeUnmount(() => {
+		document.removeEventListener("click", handleClickOutside);
+	});
+</script>
