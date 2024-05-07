266ab0284b882840c6476a0e6f8b02c0ea2d520a
bikathi <bikathimartin@outlook.com>
2024-05-07 09:38:26 +0300

    Create roles declaration for valuation side

---
+++ types/roles.ts
@@ -0,0 +1,14 @@
+const availableRoles: readonly any[] = [
+	{
+		text: "Is A Normal User",
+		role: "normal",
+		id: "normal-role",
+	},
+	{
+		text: "Is An Admin",
+		role: "admin",
+		id: "admin-role",
+	},
+];
+
+export default availableRoles;
