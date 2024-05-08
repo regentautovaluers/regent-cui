07fc5f3fb760c4fd574ac7f314fa6536a44d5fa8
bikathi <bikathimartin@outlook.com>
2024-05-08 09:56:44 +0300

    Update UI of navigation cards

--- components/navigation/DashboardHomepageNavCards.vue
+++ components/navigation/DashboardHomepageNavCards.vue
@@ -2,7 +2,8 @@
 	<NuxtLink
 		:to="{ name: componentProps.toName }"
 		class="inline-flex flex-col items-center space-y-2">
-		<div class="p-3 bg-blue-600 size-20 rounded-[2rem] flex items-center justify-center">
+		<div
+			class="p-3 bg-[#f6f9f2] border size-24 rounded-[2rem] flex items-center justify-center">
 			<img
 				:src="componentProps.imageLink"
 				alt="Link Icon"
