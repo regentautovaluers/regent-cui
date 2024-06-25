ebc69c62de22f6567ea3ce76fc026e37319e1ef2
bikathi <bikathimartin@outlook.com>
2024-06-25 09:22:47 +0300

    Connect logic to add new user to endpoint

--- components/forms/AddNewCorpUser.vue
+++ components/forms/AddNewCorpUser.vue
@@ -1,110 +1,120 @@
 <template>
 	<form
-		class="rounded-lg shadow-lg px-6 pb-6"
+		class="rounded-2xl shadow px-6 py-6 border"
 		@submit.prevent="createUserAccount">
-		<h1 class="font-semibold text-lg">Add A New User</h1>
-		<div class="flex items-center w-full">
-			<label class="w-1/6">Full Name</label>
-			<div class="flex flex-grow space-x-4">
+		<h1 class="font-semibold text-lg my-b">Add A New User</h1>
+		<div class="flex flex-col">
+			<label class="font-bold text-gray-500">Full Name</label>
+			<div class="flex w-full space-x-4">
 				<input
 					type="text"
 					id="first-name"
-					class="py-3 px-4 h-[3.5rem] block w-1/2 border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g John"
+					class="generic-input"
+					placeholder="John"
 					required
 					v-model="firstName" />
 				<input
 					type="text"
 					id="other-name"
-					class="py-3 px-4 h-[3.5rem] block w-1/2 border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g Doe"
+					class="generic-input"
+					placeholder="Doe"
 					required
 					v-model="otherName" />
 			</div>
 		</div>
-		<div class="flex items-center w-full mt-6">
-			<label class="w-1/6">Email</label>
+		<div class="flex flex-col mt-3">
+			<label class="font-bold text-gray-500">Email</label>
 			<div class="flex flex-grow">
 				<input
 					type="email"
 					id="email"
-					class="py-3 px-4 h-[3.5rem] block w-full border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g johndoe@corporate.email.com"
+					class="generic-input"
+					placeholder="johndoe@corpemail.com"
 					required
 					v-model="email" />
 			</div>
 		</div>
-		<div class="flex items-center w-full mt-6">
-			<label class="w-1/6">Phone</label>
+		<div class="flex flex-col mt-3">
+			<label class="font-bold text-gray-500">Phone Number</label>
 			<div class="flex flex-grow">
 				<input
 					type="text"
 					id="phone"
-					class="py-3 px-4 h-[3.5rem] block w-full border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g 0712345678"
+					class="generic-input"
+					placeholder="254712345678"
 					required
 					v-model="phoneNumber" />
 			</div>
 		</div>
-		<div class="flex items-center mt-6">
-			<label class="w-1/6">Password</label>
+		<div class="flex flex-col mt-3">
+			<label class="font-bold text-gray-500">Password</label>
 			<div class="flex flex-grow">
 				<input
 					type="password"
 					id="new-password"
-					class="py-3 px-4 h-[3.5rem] block w-full border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g gnarly_squirell@123"
+					class="generic-input"
+					placeholder="gnarly_squirell@123"
 					required
 					v-model="password" />
 			</div>
 		</div>
-		<div class="flex items-center mt-6">
-			<label class="w-1/6">Role In Company</label>
+		<div class="flex flex-col mt-3">
+			<label class="font-bold text-gray-500">Role In Company</label>
 			<div class="flex flex-grow">
 				<input
 					type="text"
 					id="role"
-					class="py-3 px-4 h-[3.5rem] block w-full border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-					placeholder="e.g Underwriter"
+					class="generic-input"
+					placeholder="Underwriter"
 					required
 					v-model="companyRole" />
 			</div>
 		</div>
-		<div class="flex items-center mt-6">
-			<label class="w-1/6">User Privilege</label>
+		<div class="flex flex-col mt-3">
+			<label class="font-bold text-gray-500">User Privilege</label>
 			<div class="flex flex-grow space-x-4">
 				<label
-					class="flex p-3 w-1/2 bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500"
-					v-for="(item, index) in availableRoles"
-					:key="index"
-					:for="item.id">
-					<span class="text-sm text-gray-500 dark:text-gray-400">{{
-						item.text
-					}}</span>
+					class="flex items-center py-3 px-4 w-1/2 bg-white border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500">
+					<span class="text-gray-500"> Is A Normal User </span>
 					<input
 						type="radio"
-						:value="item.role"
-						class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500"
-						:id="item.id"
-						v-model="userPrivilege" />
+						value="role_corp_norm"
+						class="radio-buttons"
+						id="normal"
+						checked
+						v-model="userRole" />
+				</label>
+				<label
+					class="flex items-center py-3 px-4 w-1/2 bg-white border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500">
+					<span class="text-gray-500"> Is An Admin </span>
+					<input
+						type="radio"
+						value="role_corp_admin"
+						class="radio-buttons"
+						id="admin"
+						v-model="userRole" />
 				</label>
 			</div>
 		</div>
-		<button
-			type="submit"
-			class="py-3 px-4 mt-7 lg:w-1/3 text-lg h-16 items-center gap-x-2 font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-			<span v-if="!formSubmissionLoading">Add User Account</span>
-			<div
-				v-if="formSubmissionLoading"
-				class="animate-spin inline-block size-5 border-[3px] border-white border-current border-t-transparent text-gray-800 rounded-full"
-				role="status"
-				aria-label="loading" />
-		</button>
+		<div class="flex w-full mt-2 lg:w-1/3 items-center justify-center">
+			<button
+				v-if="!formSubmissionLoading"
+				type="submit"
+				class="form-submit">
+				Create User Account
+			</button>
+			<looping-rhombuses-spinner
+				v-else
+				:animation-duration="2000"
+				:rhombus-size="20"
+				color="#2563eb" />
+		</div>
 	</form>
 </template>
 
 <script setup lang="ts">
-	import availableRoles from "~/types/roles";
+	import { LoopingRhombusesSpinner } from "epic-spinners";
+
 	const formSubmissionLoading = ref(false);
 	const firstName: Ref<string> = ref("");
 	const otherName: Ref<string> = ref("");
@@ -112,49 +122,44 @@
 	const phoneNumber: Ref<string> = ref("");
 	const password: Ref<string> = ref("");
 	const companyRole: Ref<string> = ref("");
-	const companyBranch: Ref<string> = ref("");
-	const userPrivilege: Ref<string> = ref("");
+	const userRole: Ref<string> = ref("");
 	const runtimeConfig = useRuntimeConfig();
 	const { getPrincipal } = useAuth();
 	const { openToast } = useToast();
-	const userName: ComputedRef<string> = computed(
-		() => `${firstName.value} ${otherName.value}`
-	);
+
+	watch(userRole, (newRole) => {
+		console.log("new role: ", newRole);
+	});
+
+	watch(phoneNumber, (newNumber) => {
+		if (newNumber.startsWith("0") || newNumber.startsWith("+254")) {
+			phoneNumber.value = newNumber.replace(/^(\+254|0)/, "254");
+		}
+	});
 
 	async function createUserAccount(): Promise<void> {
 		formSubmissionLoading.value = true;
 		try {
-			await $fetch("/ava/api/corp-users/add", {
+			await $fetch("/api/v1/auth/corp-account/signup", {
+				baseURL: runtimeConfig.public.VALUATION_BASE_URL,
 				method: "POST",
 				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
+					Accept: "application/json",
+					"Content-Type": "application/json",
 				},
-				query: {
-					corp: getPrincipal.value.corpId,
-					name: userName.value,
+				body: JSON.stringify({
+					firstName: firstName.value,
+					lastName: otherName.value,
 					email: email.value,
+					phoneNumber: phoneNumber.value,
 					password: password.value,
-					access_level: userPrivilege.value,
-					phone: phoneNumber.value,
-					branch: companyBranch.value,
-					role: companyRole.value,
-					added_by: getPrincipal.value.username,
-					created_by: "ava",
-					uname: runtimeConfig.app.VALUATION_BASE_UNAME,
-					pwd: runtimeConfig.app.VALUATION_BASE_PASS,
-				},
-				async onResponse({ response }) {
-					if (response.status !== 200) {
-						throw new Error("Failed to create user account!");
-					}
-
-					openToast(
-						"Successfully created user account. Reloading",
-						"success"
-					);
-
-					// reload page to clear form
-					location.reload();
+					profilePicture: null,
+					corporateId: getPrincipal.value.corpId,
+					userRoles: [userRole.value],
+				}),
+				onResponse({ response }) {
+					console.info("Response data: ", response._data);
+					console.info("Response status: ", response.status);
 				},
 			});
 		} catch (error) {
