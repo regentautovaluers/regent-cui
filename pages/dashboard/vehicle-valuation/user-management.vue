f057742156536a1c8bb03f8f6ccda36c39297d8d
bikathi <bikathimartin@outlook.com>
2024-06-25 12:19:08 +0300

    Improve UI structuring for user management page

--- pages/dashboard/vehicle-valuation/user-management.vue
+++ pages/dashboard/vehicle-valuation/user-management.vue
@@ -2,16 +2,16 @@
 	<div class="py-10 responsive-view h-fit">
 		<h1 class="text-xl my-5 md:text-3xl font-semibold">Actions</h1>
 		<div class="flex space-x-4">
-			<div class="h-full flex flex-col space-y-1 w-1/6 min-w-1/6">
+			<div class="h-full flex flex-col space-y-1 w-[12%] min-w-1/6">
 				<NuxtLink
 					v-for="(link, index) in [
 						{
-							text: 'Add New User',
-							to: 'valuation-users-add-user',
-						},
-						{
 							text: 'Manage All Users',
 							to: 'valuation-manage-users',
+						},
+						{
+							text: 'Add New User',
+							to: 'valuation-users-add-user',
 						},
 						{
 							text: 'User Details',
@@ -29,7 +29,7 @@
 					>{{ link.text }}</NuxtLink
 				>
 			</div>
-			<div class="h-full w-[65%]">
+			<div class="h-full w-[70%]">
 				<NuxtPage />
 			</div>
 			<div class="h-full flex-grow flex justify-center items-center">
