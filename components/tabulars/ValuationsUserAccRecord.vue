be7c39d2a6a51a0f8673f13e3204e0fbaf09eb52
bikathi <bikathimartin@outlook.com>
2024-06-25 12:29:41 +0300

    Complete UI for populating user accounts

--- components/tabulars/ValuationsUserAccRecord.vue
+++ components/tabulars/ValuationsUserAccRecord.vue
@@ -1,25 +1,40 @@
 <template>
 	<tr class="hover:shadow-lg">
-		<td class="px-6 py-6 whitespace-nowrap font-semibold text-pink-600">
-			{{ componentProps.nameOfUser }}
-		</td>
-		<td class="px-6 py-6 whitespace-nowrap text-gray-600 font-semibold">
-			{{ componentProps.username }}
-		</td>
-		<td class="px-6 py-6 whitespace-nowrap text-gray-600 font-semibold">
-			{{ componentProps.userLevel }}
+		<td class="px-6 py-4 whitespace-nowrap font-semibold text-pink-600">
+			{{ props.username }}
 		</td>
 		<td
-			class="px-6 py-6 whitespace-nowrap text-center font-semibold text-blue-500">
-			{{ componentProps.isActive }}
+			class="px-6 py-4 whitespace-nowrap text-xs text-gray-800 inline-flex flex-col space-y-1">
+			<span
+				class="bg-gray-200 text-gray-500 font-semibold px-2 rounded-full"
+				>{{
+					props.userEmail === null ? "Email N/A" : props.userEmail
+				}}</span
+			>
+			<span
+				class="bg-gray-200 text-gray-500 font-semibold px-2 rounded-full w-fit"
+				>{{
+					props.phoneNumber === null ? "Phone N/A" : props.phoneNumber
+				}}</span
+			>
 		</td>
-		<td
-			class="px-6 py-6 whitespace-nowrap text-end font-semibold text-green-500">
-			{{ componentProps.accCreationDate }}
+		<td class="px-6 py-4 whitespace-nowrap text-gray-600 font-semibold">
+			{{ determinePrivilege() }}
 		</td>
-		<td
-			class="px-6 py-6 whitespace-nowrap text-ellipsis overflow-hidden text-end font-semibold text-pink-500">
-			{{ componentProps.accCreatedBy }}
+		<td class="px-6 py-4 whitespace-nowrap text-gray-600 font-semibold">
+			{{ props.corporateRole }}
+		</td>
+		<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+			<span class="inline-flex items-center space-x-1">
+				<div
+					class="size-3 rounded-full"
+					:class="
+						props.isActive === true ? 'bg-green-500' : 'bg-red-600'
+					" />
+				<span>{{
+					props.isActive === true ? "Active" : "Inactive"
+				}}</span>
+			</span>
 		</td>
 		<td>
 			<div
@@ -51,44 +66,12 @@
 							:to="{
 								name: 'valuation-update-user-details',
 								query: {
-									'corpuid': componentProps.corpUserId,
+									corpuid: props.corpId,
 								},
 							}"
 							class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
-							Update Account Details
+							Update Details
 						</NuxtLink>
-						<button
-							v-if="componentProps.isActive === 'y'"
-							class="w-full flex justify-between items-center gap-x-3.5 py-2 px-3 rounded-lg bg-red-500 hover:bg-red-600 focus:outline-none text-white"
-							@click="
-								deactivateAccount(
-									componentProps.corpUserId,
-									componentProps.corp
-								)
-							">
-							<span>Deactivate Account</span>
-							<div
-								v-if="deActivateAccountLoading"
-								class="animate-spin inline-block size-5 border-[3px] border-white border-current border-t-transparent text-gray-800 rounded-full"
-								role="status"
-								aria-label="loading" />
-						</button>
-						<button
-							v-if="componentProps.isActive === 'n'"
-							class="w-full flex justify-between items-center gap-x-3.5 py-2 px-3 rounded-lg bg-green-500 hover:bg-green-600 focus:outline-none text-white"
-							@click="
-								activateAccount(
-									componentProps.corpUserId,
-									componentProps.corp
-								)
-							">
-							<span>Activate Account</span>
-							<div
-								v-if="activateAccountLoading"
-								class="animate-spin inline-block size-5 border-[3px] border-white border-current border-t-transparent text-gray-800 rounded-full"
-								role="status"
-								aria-label="loading" />
-						</button>
 					</div>
 				</div>
 			</div>
@@ -97,23 +80,27 @@
 </template>
 
 <script setup lang="ts">
-	const componentProps = defineProps<{
-		corpUserId: string;
-		corp: string;
-		nameOfUser: string;
+	const props = defineProps<{
+		userId: string;
+		corpId: string;
 		username: string;
-		userLevel: string;
-		isActive: string;
-		accCreationDate: string;
-		accCreatedBy: string;
+		userEmail: string;
+		phoneNumber: string;
+		privilege: string[];
+		corporateRole: string;
+		isActive: boolean;
 	}>();
 
 	const isDropdownVisible = ref(false);
 	const dropdownContainer = ref(null);
-	const activateAccountLoading: Ref<boolean> = ref(false);
-	const deActivateAccountLoading: Ref<boolean> = ref(false);
-	const { openToast } = useToast();
-	const runtimeConfig = useRuntimeConfig();
+
+	const determinePrivilege = (): string => {
+		if (props.privilege.includes("ROLE_CORP_ADMIN")) {
+			return "Admin User";
+		} else if(props.privilege.includes("ROLE_CORP_NORM")) {
+			return "Normal User"
+		}
+	};
 
 	function toggleDropdown() {
 		isDropdownVisible.value = !isDropdownVisible.value;
@@ -122,86 +109,4 @@
 	function closeDropdown() {
 		isDropdownVisible.value = false;
 	}
-
-	function handleClickOutside(event: MouseEvent) {
-		if (!event.target) return;
-		// Check if the click is outside the dropdown container
-		if (!dropdownContainer.value.contains(event.target as Node)) {
-			closeDropdown();
-		}
-	}
-
-	async function deactivateAccount(
-		corpUID: string,
-		corpName: string
-	): Promise<void> {
-		deActivateAccountLoading.value = true;
-		try {
-			await $fetch("/ava/api/corp-users/deactivate", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				query: {
-					uname: runtimeConfig.app.VALUATION_BASE_UNAME,
-					pwd: runtimeConfig.app.VALUATION_BASE_PASS,
-					corp: corpName,
-					corp_user_id: corpUID,
-				},
-				async onResponse({ response }) {
-					if (response.status !== 200) {
-						throw new Error("Failed to deactivate user account!");
-					}
-
-					openToast("Account deactivation successfull", "success");
-				},
-			});
-		} catch (error) {
-			console.log("An error occured: ", error);
-			openToast("Failed to deactivate account. Try again!", "danger");
-		} finally {
-			deActivateAccountLoading.value = false;
-		}
-	}
-
-	async function activateAccount(
-		corpUID: string,
-		corpName: string
-	): Promise<void> {
-		activateAccountLoading.value = true;
-		try {
-			await $fetch("/ava/api/corp-users/activate", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				query: {
-					uname: runtimeConfig.app.VALUATION_BASE_UNAME,
-					pwd: runtimeConfig.app.VALUATION_BASE_PASS,
-					corp: corpName,
-					corp_user_id: corpUID,
-				},
-				async onResponse({ response }) {
-					if (response.status !== 200) {
-						throw new Error("Failed to activate user account!");
-					}
-
-					openToast("Account activation successfull", "success");
-				},
-			});
-		} catch (error) {
-			console.log("An error occured: ", error);
-			openToast("Failed to activate account. Try again!", "danger");
-		} finally {
-			activateAccountLoading.value = false;
-		}
-	}
-
-	onMounted(() => {
-		document.addEventListener("click", handleClickOutside);
-	});
-
-	onBeforeUnmount(() => {
-		document.removeEventListener("click", handleClickOutside);
-	});
 </script>
