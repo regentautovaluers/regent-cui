94a342793210d7db6c58d20785e806d1ff9f969c
bikathi <bikathimartin@outlook.com>
2024-07-15 08:24:05 +0300

    Repurpose loading indicator for form submission loading indicators

--- components/misc/LoadingIndicator.vue
+++ components/misc/LoadingIndicator.vue
@@ -2,24 +2,27 @@
 	<!-- Progress -->
 	<div class="w-full flex justify-center">
 		<div
-		class="flex h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700"
-		:class="componentProps.barLength"
-		role="progressbar"
-		aria-valuenow="25"
-		aria-valuemin="0"
-		aria-valuemax="100">
-		<div
-			class="flex flex-col justify-center rounded-full overflow-hidden bg-blue-600 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500"
-			style="width: 100%; animation: scroll 1.5s linear infinite"></div>
+			class="flex h-2 rounded-lg  overflow-hidden"
+			:class="componentProps.injectClasses"
+			role="progressbar"
+			aria-valuenow="25"
+			aria-valuemin="0"
+			aria-valuemax="100">
+			<div
+				class="bg-gray-200"
+				style="
+					width: 80%;
+					animation: scroll 1.8s linear infinite;
+				"></div>
+		</div>
 	</div>
-	</div>
-	
+
 	<!-- End Progress -->
 </template>
 
 <script setup lang="ts">
 	const componentProps = defineProps<{
-		barLength: string;
+		injectClasses: string;
 	}>();
 </script>
 
@@ -33,6 +36,15 @@
 		}
 		100% {
 			transform: translateX(-100%);
+		}
+	}
+
+	@keyframes zoom {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(100%);
 		}
 	}
 </style>
