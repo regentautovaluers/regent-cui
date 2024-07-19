2eb381a24ec7f02738d409cfcf0c9272a3f15e65
bikathi <bikathimartin@outlook.com>
2024-07-19 12:08:04 +0300

    Import icons for use in the report of a service request

---
+++ components/icons/PickupLocationIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		viewBox="0 0 24 24"
+		:class="props.classes">
+		<path
+			fill="white"
+			d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7" />
+	</svg>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps({
+		classes: { required: true, type: Array<string> },
+	});
+</script>
