093860ad708a0f09698a04f21928d73451e0413b
bikathi <bikathimartin@outlook.com>
2024-08-24 21:23:55 +0300

    Create re-usable logic to show toast messages

---
+++ composables/useToast.ts
@@ -0,0 +1,33 @@
+import { createToast, type ToastType } from 'mosha-vue-toastify';
+import 'mosha-vue-toastify/dist/style.css';
+import { type ToastConfigOptions } from '~/types';
+
+const useToast = (
+	message: string,
+	{
+		timeout,
+		type,
+		showCloseButton,
+		transition,
+		hideProgressBar,
+		showIcon,
+		position,
+	}: ToastConfigOptions,
+) => {
+	createToast(
+		{
+			title: message,
+		},
+		{
+			timeout: timeout ?? 3000,
+			type: type as ToastType,
+			showCloseButton: showCloseButton ?? false,
+			transition: transition ?? 'slide',
+			hideProgressBar: hideProgressBar ?? true,
+			showIcon: showIcon ?? false,
+			position: position ?? 'top-center',
+		},
+	);
+};
+
+export default useToast;
