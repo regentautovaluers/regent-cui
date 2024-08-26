1c58ad3a7496fedb64daa1c373ee5d0cea35c7e5
bikathi <bikathimartin@outlook.com>
2024-08-26 10:40:19 +0300

    Add ability to view a member's vehicles

--- components/misc/MemberVehiclesCard.vue
+++ components/misc/MemberVehiclesCard.vue
@@ -2,25 +2,36 @@
 	<div
 		class="flex h-fit w-[22rem] min-w-[22rem] flex-col overflow-clip rounded-lg bg-blue-600 transition-colors duration-300 hover:bg-blue-700">
 		<div class="flex h-12 items-center pl-4 text-xl font-extrabold text-white">
-			<h1>KAU 2221</h1>
+			<h1>{{ props.regNo }}</h1>
 		</div>
 		<div class="my-4 px-4 text-white">
-			<h1 class="mb-2 text-lg font-semibold">Roadside Assistance</h1>
+			<h1 class="mb-2 text-lg font-semibold">{{ props.membershipType }}</h1>
 			<div class="grid grid-cols-2">
 				<div>
 					<h1 class="w-fit text-gray-400">Joined</h1>
-					<h2 class="font-semibold">14/13/2023</h2>
+					<h2 class="font-semibold">{{ props.dateRegistered }}</h2>
 				</div>
 				<div>
 					<h1 class="w-fit text-gray-400">Expiry Date</h1>
-					<h2 class="font-semibold">12/13/2024</h2>
+					<h2 class="font-semibold">{{ props.expirationDate }}</h2>
 				</div>
 			</div>
 		</div>
 		<div
 			class="flex h-12 items-center space-x-2 rounded-b-lg bg-gray-600 bg-opacity-40 pl-4 font-semibold text-white">
 			<div class="size-3 rounded-full bg-green-500" />
-			<h1>Active</h1>
+			<h1>{{ props.membershipStatus }}</h1>
 		</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+	const props = defineProps({
+		membershipId: { required: true, type: Number },
+		regNo: { required: true, type: String },
+		membershipType: { required: true, type: String },
+		dateRegistered: { required: true, type: String },
+		expirationDate: { required: true, type: String },
+		membershipStatus: { required: true, type: String },
+	});
+</script>
--- pages/console/ava-memberships/index.vue
+++ pages/console/ava-memberships/index.vue
@@ -13,7 +13,7 @@
 						>
 						<HandshakeIcon />
 					</h1>
-					<h2>You Have 110 Registered Members.</h2>
+					<h2>You Have {{ totalNumber }} Registered Members.</h2>
 				</div>
 			</div>
 			<NuxtLink
@@ -57,9 +57,14 @@
 					<!-- the table itself -->
 					<div class="my-4 flex-grow">
 						<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-							<table class="w-full text-left text-sm text-gray-500">
+							<table class="w-full text-left text-gray-500">
 								<thead class="bg-gray-100 text-xs uppercase text-gray-700">
 									<tr>
+										<th
+											scope="col"
+											class="table-headers">
+											Membership Number
+										</th>
 										<th
 											scope="col"
 											class="table-headers">
@@ -68,7 +73,7 @@
 										<th
 											scope="col"
 											class="table-headers">
-											Membership Category
+											Category
 										</th>
 										<th
 											scope="col"
@@ -82,32 +87,35 @@
 										</th>
 										<th
 											scope="col"
-											class="table-headers">
-											Email
-										</th>
-										<th
-											scope="col"
 											class="table-headers" />
 									</tr>
 								</thead>
 								<tbody>
 									<tr
 										class="border-b bg-white hover:bg-gray-100"
-										v-for="a in 10"
-										:key="a">
+										v-for="(member, index) in membersList"
+										:key="index">
 										<td
 											scope="row"
-											class="whitespace-nowrap p-6 font-medium text-gray-900">
-											Apple MacBook Pro 17"
+											class="whitespace-nowrap p-6 font-medium text-gray-600">
+											{{ member.membership_number }}
 										</td>
-										<td class="p-6">Silver</td>
-										<td class="p-6">Laptop</td>
-										<td class="p-6">$2999</td>
-										<td class="p-6">$2999</td>
+										<td
+											scope="row"
+											class="whitespace-nowrap p-6 font-semibold text-gray-600">
+											{{ member.full_name }}
+										</td>
+										<td class="p-6 text-blue-600">
+											{{ stringToTitleCase(member.category) }}
+										</td>
+										<td class="p-6 font-semibold text-pink-600">
+											{{ member.membershipVehicleCount }}
+										</td>
+										<td class="p-6">{{ member.phone_number }}</td>
 										<td class="flex items-center justify-end p-6">
 											<button
-												:id="'dropdownTopButton' + a"
-												:data-dropdown-toggle="'dropdownTop' + a"
+												:id="'dropdownTopButton' + index"
+												:data-dropdown-toggle="'dropdownTop' + index"
 												data-dropdown-placement="top"
 												type="button">
 												<svg
@@ -121,7 +129,7 @@
 											</button>
 											<!-- Dropdown menu -->
 											<div
-												:id="'dropdownTop' + a"
+												:id="'dropdownTop' + index"
 												class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg border bg-white shadow-md">
 												<ul
 													class="py-2 text-sm text-gray-500"
@@ -139,8 +147,12 @@
 														<button
 															class="block w-full px-4 py-2 text-center hover:bg-gray-100"
 															type="button"
-															data-modal-target="view-member-vehicles-modal"
-															data-modal-toggle="view-member-vehicles-modal">
+															@click="
+																() => {
+																	getMemberVehicles(member.id);
+																	isGetVehiclesModalOpen = true;
+																}
+															">
 															View Vehicles
 														</button>
 													</li>
@@ -156,7 +168,7 @@
 					<!-- page controls -->
 					<div class="flex h-12 items-center justify-between">
 						<h1 class="text-sm font-semibold text-gray-500 md:text-base">
-							Showing X of Y pages.
+							Showing {{ currentPage }} of {{ totalPages }} pages.
 						</h1>
 						<div class="h-full space-x-2 md:space-x-4">
 							<button class="table-page-buttons">Previous</button>
@@ -243,21 +255,134 @@
 	<!-- Edit Member details modal -->
 	<EditMemberDetailsModal />
 
-	<!-- Add member vehicles -->
-	<ViewMemberVehiclesModal />
+	<!-- View Member Vehicles modal -->
+	<ParentModal
+		v-if="isGetVehiclesModalOpen"
+		modalId="member-vehicles"
+		modalTitle="Member Vehicles"
+		class="relative p-3 py-2 md:p-4"
+		@close-modal="
+			() => {
+				isGetVehiclesModalOpen = false;
+				memberVehicles = [];
+			}
+		">
+		<div
+			class="flex h-fit space-x-3 overflow-x-auto"
+			ref="scrollEl"
+			style="
+				::-webkit-scrollbar-thumb {
+					border-radius: 10px;
+				}
+
+				/* Hide scrollbar for Chrome, Safari and Opera */
+				::-webkit-scrollbar {
+					display: none;
+				}
+
+				/* Hide scrollbar for IE, Edge and Firefox */
+				.no-scrollbar {
+					-ms-overflow-style: none; /* IE and Edge */
+					scrollbar-width: none; /* Firefox */
+				}
+			">
+			<MemberVehiclesLoader
+				v-if="fetchMemberVehiclesLoading"
+				v-for="a in 2"
+				:key="a" />
+			<MemberVehiclesCard
+				v-else
+				v-for="(vehicle, index) in memberVehicles"
+				:key="index"
+				:membership-id="vehicle.id"
+				:reg-no="vehicle.registration"
+				:membership-type="stringToSentenceCase(vehicle.membershipType.membership_name)"
+				:date-registered="formatServerProvidedDate(vehicle.start_date)"
+				:expiration-date="formatServerProvidedDate(vehicle.end_date)"
+				:membership-status="stringToTitleCase(vehicle.membership_status)" />
+		</div>
+		<!-- left button -->
+		<button
+			type="button"
+			class="group absolute start-0 top-0 z-30 h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+			@click="x -= 352"
+			:class="arrivedState.left ? 'disabled' : null"
+			v-if="memberVehicles.length > 1">
+			<span
+				class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/50 outline-none ring-white group-focus:ring-4">
+				<svg
+					class="h-4 w-4 text-white rtl:rotate-180"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 6 10">
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 1 1 5l4 4" />
+				</svg>
+				<span class="sr-only">Scroll Left</span>
+			</span>
+		</button>
+
+		<!-- right button -->
+		<button
+			type="button"
+			class="group absolute end-0 top-0 z-30 h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+			:class="arrivedState.right ? 'disabled' : null"
+			@click="x += 352"
+			v-if="memberVehicles.length > 1">
+			<span
+				class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/50 outline-none ring-white group-focus:ring-4">
+				<svg
+					class="h-4 w-4 text-white rtl:rotate-180"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 6 10">
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="m1 9 4-4-4-4" />
+				</svg>
+				<span class="sr-only">Scroll Right</span>
+			</span>
+		</button>
+	</ParentModal>
 </template>
 
 <script setup lang="ts">
 	import { avaServices } from '~/data';
+	import { useScroll } from '@vueuse/core';
 
 	definePageMeta({
 		name: 'ava-memberships-home',
 		layout: 'console-layout',
 	});
 
+	const scrollEl = ref<HTMLElement | null>(null);
+	const { x, arrivedState } = useScroll(scrollEl, {
+		behavior: 'smooth',
+	});
 	const activeDescriptionIndex: Ref<number> = ref(0);
 	const profilePicture: Ref<string> = ref('');
 	const { getPrincipal } = useAuth();
+	const isGetVehiclesModalOpen: Ref<boolean> = ref(false);
+	const {
+		currentPage,
+		membersList,
+		totalNumber,
+		totalPages,
+		fetchMembershipsStatus,
+		fetchMemberVehiclesLoading,
+		memberVehicles,
+		getMemberVehicles,
+		refreshMembers,
+	} = useAVAMemberships();
 
 	onMounted(() => (profilePicture.value = getPrincipal.value.profilePicture));
 </script>
