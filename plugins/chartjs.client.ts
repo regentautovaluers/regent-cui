6f73296d5b63895d21801b0784ece84fe6c09ac8
bikathi <bikathimartin@outlook.com>
2024-08-28 11:16:05 +0300

    Create plugin to manage importing of ChartJS components

---
+++ plugins/chartjs.client.ts
@@ -0,0 +1,24 @@
+import {
+	Chart,
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale,
+	ArcElement,
+} from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+export default defineNuxtPlugin(() => {
+	Chart.register(
+		CategoryScale,
+		LinearScale,
+		BarElement,
+		Title,
+		Tooltip,
+		Legend,
+		ArcElement,
+		ChartDataLabels,
+	);
+});
