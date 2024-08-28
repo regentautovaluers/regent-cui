e5fb9a65b1f19ad3cdd4c0ce0255923fe793e664
bikathi <bikathimartin@outlook.com>
2024-08-28 13:01:38 +0300

    Create routes to show towing and non-towing reports

---
+++ pages/console/roadside-assistance/report/[service_type]/expanded/[id].vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Here you will see towing reports</h1>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: 'ra-expanded-report',
+		layout: 'console-layout',
+	});
+</script>
---
+++ pages/console/roadside-assistance/report/[service_type]/minimized/[id].vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Here, you will see the non-towing report view</h1>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: 'ra-minimized-report',
+		layout: 'console-layout',
+	});
+</script>
