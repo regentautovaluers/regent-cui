c0f4e389db9fcd5ccc52215ddb8b1f4dfa8f6a54
bikathi <bikathimartin@outlook.com>
2024-09-03 10:05:29 +0300

    Add icons related to the Towing report

---
+++ components/icons/ToolkitIcon.vue
@@ -0,0 +1,19 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		viewBox="0 0 24 24"
+		:class="props.classes">
+		<path
+			:fill="props.color"
+			d="m22.7 19l-9.1-9.1c.9-2.3.4-5-1.5-6.9c-2-2-5-2.4-7.4-1.3L9 6L6 9L1.6 4.7C.4 7.1.9 10.1 2.9 12.1c1.9 1.9 4.6 2.4 6.9 1.5l9.1 9.1c.4.4 1 .4 1.4 0l2.3-2.3c.5-.4.5-1.1.1-1.4" />
+	</svg>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps({
+		classes: { required: false, type: String },
+		color: { required: true, type: String },
+	});
+</script>
