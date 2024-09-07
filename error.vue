4137a5e5b1e54f87933c10711f970e21b3257aa7
bikathi <bikathimartin@outlook.com>
2024-09-07 10:05:35 +0300

    Create a error fallback page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+	<main class="flex h-screen flex-col items-center justify-center">
+		<h1 class="text-5xl font-semibold text-gray-500">{{ props.error?.statusCode }}</h1>
+		<h1 class="font-semibold italic text-gray-500">
+			{{ props.error ? props.error.message : 'Something Went Wrong!' }}
+		</h1>
+	</main>
+</template>
+
+<script setup lang="ts">
+	import type { NuxtError } from '#app';
+
+	const props = defineProps({
+		error: Object as () => NuxtError,
+	});
+</script>
