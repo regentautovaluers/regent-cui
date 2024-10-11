3016f5912436d8fe9ef100e4849bd31e27e5f577
bikathi <bikathimartin@outlook.com>
2024-10-11 10:22:01 +0300

    Add filter icon and styles to the 'trigger filter' button

---
+++ components/icons/FilterIcon.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1.4em"
+		height="1.4em"
+		viewBox="0 0 24 24"
+		:class="props.classes">
+		<path
+			:fill="props.color"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M4.5 7h15M7 12h10m-7 5h4" />
+	</svg>
+</template>
+
+<script lang="ts" setup>
+	const props = defineProps({
+		classes: { required: false, type: String },
+		color: { required: false, type: String },
+	});
+</script>
