170aadf78d634a61da164740d3916a808d3f2db2
bikathi <bikathimartin@outlook.com>
2024-11-15 11:35:01 +0300

    Show red indicator for inactive membership vehicles

--- components/misc/MemberVehiclesCard.vue
+++ components/misc/MemberVehiclesCard.vue
@@ -19,7 +19,9 @@
 		</div>
 		<div
 			class="flex h-12 items-center space-x-2 rounded-b-lg bg-gray-600 bg-opacity-40 pl-4 font-semibold text-white">
-			<div class="size-3 rounded-full bg-green-500" />
+			<div
+				class="rounded-full size-3"
+				:class="props.membershipStatus === 'Active' ? 'bg-green-500' : 'bg-red-500'" />
 			<h1>{{ props.membershipStatus }}</h1>
 		</div>
 	</div>
