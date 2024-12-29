1b5fbf89fd308f3f1885ced78b3a3ef6ccd7d2f7
bikathi-martin <bikathimartin@outlook.com>
2024-12-29 16:46:40 +0300

    Create page that will show valuation report

---
+++ pages/console/vehicle-valuation/report/[valuation_id].vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>valuation report</h1>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: 'vehicle-valuation-report',
+		layout: 'console-layout',
+	});
+</script>
