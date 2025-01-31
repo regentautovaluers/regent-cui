26c0fdf748652720159273a7bbf698cc1a5d91c3
bikathi-martin <bikathimartin@outlook.com>
2025-01-31 12:43:52 +0300

    Complete basic UI for rendering valuation report

--- pages/console/vehicle-valuation/report/[valuation_id].vue
+++ pages/console/vehicle-valuation/report/[valuation_id].vue
@@ -1,5 +1,232 @@
 <template>
-	<h1>valuation report</h1>
+	<div class="grid h-fit grid-cols-2 gap-10">
+		<div class="rounded-lg border-[.5px] shadow-md outline-none">
+			<div class="flex items-center justify-between p-10">
+				<div class="space-y-3">
+					<h2 class="text-gray-600">BK-JAN24-KA34-FDS900</h2>
+					<h1 class="text-4xl font-bold">KDG 188T</h1>
+				</div>
+				<div>
+					<a
+						href="#"
+						class="generic-form-submit text-base"
+						>Download Report</a
+					>
+				</div>
+			</div>
+			<hr class="border-[.5px]" />
+			<div class="flex h-fit items-center p-10">
+				<img
+					src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=3939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt="Vehicle Image"
+					class="h-52 w-64 rounded-md border shadow-sm" />
+				<div class="flex h-52 flex-grow flex-col justify-end px-5">
+					<span class="text-2xl font-semibold">Christine Gathoni</span>
+					<span class="text-xl tracking-wide text-gray-600">Toyota Voxy ZRR80</span>
+					<span class="text-lg text-gray-500">Station Wagon Auto</span>
+					<span class="text-lg text-gray-500">ABCD/1234/RTYU/XYZ</span>
+				</div>
+			</div>
+			<div class="grid grid-cols-4 gap-x-2 p-10">
+				<div>
+					<h1 class="text-lg font-semibold">Client Name</h1>
+					<span class="text-gray-500">Test Name</span>
+				</div>
+				<div>
+					<h1 class="text-lg font-semibold">Chassis Number</h1>
+					<span class="text-gray-500">Test Name</span>
+				</div>
+				<div>
+					<h1 class="text-lg font-semibold">Engine Number</h1>
+					<span class="text-gray-500">Test Name</span>
+				</div>
+				<div>
+					<h1 class="text-lg font-semibold">Mileage</h1>
+					<span class="text-gray-500">Test Name</span>
+				</div>
+			</div>
+
+			<!-- kyc -->
+			<div class="grid grid-cols-4 gap-x-2 px-10 py-5">
+				<div class="col-span-4">
+					<h1 class="text-lg font-semibold text-gray-600">Client KYCs</h1>
+				</div>
+				<div class="rounded-lg bg-gray-100 p-3">
+					<h1 class="font-semibold text-gray-700">Authority Letter</h1>
+					<a
+						href="#"
+						class="text-sm text-blue-700"
+						>Download</a
+					>
+				</div>
+				<div class="rounded-lg bg-gray-100 p-3">
+					<h1 class="font-semibold text-gray-700">KRA PIN</h1>
+					<a
+						href="#"
+						class="text-sm text-blue-700"
+						>Download</a
+					>
+				</div>
+				<div class="rounded-lg bg-gray-100 p-3">
+					<h1 class="font-semibold text-gray-700">Registration Cert</h1>
+					<a
+						href="#"
+						class="text-sm text-blue-700"
+						>Download</a
+					>
+				</div>
+				<div class="rounded-lg bg-gray-100 p-3">
+					<h1 class="font-semibold text-gray-700">Vehicle Logbook</h1>
+					<a
+						href="#"
+						class="text-sm text-blue-700"
+						>Download</a
+					>
+				</div>
+			</div>
+		</div>
+		<div class="space-y-5 rounded-lg border-[.5px] p-10 shadow-md outline-none">
+			<div class="flex items-start space-x-4">
+				<!-- the pill -->
+				<div class="h-10 max-h-10 w-4 max-w-4 rounded-full bg-blue-500"></div>
+				<div class="h-fit flex-grow">
+					<h1 class="text-2xl font-semibold">Tyre Condition</h1>
+					<p class="mt-3 text-gray-500">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi laudantium
+						iure, eligendi illo doloribus similique! Tenetur numquam reprehenderit
+						laborum dolores nam, itaque cumque error nemo sapiente iure saepe hic
+						repellendus!
+					</p>
+				</div>
+			</div>
+			<div class="flex items-start space-x-4">
+				<!-- the pill -->
+				<div class="h-10 max-h-10 w-4 max-w-4 rounded-full bg-green-500"></div>
+				<div class="h-fit flex-grow">
+					<h1 class="text-2xl font-semibold">Extras</h1>
+					<p class="mt-3 text-gray-500">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi laudantium
+						iure, eligendi illo doloribus similique! Tenetur numquam reprehenderit
+						laborum dolores nam, itaque cumque error nemo sapiente iure saepe hic
+						repellendus!
+					</p>
+				</div>
+			</div>
+			<div class="flex items-start space-x-4">
+				<!-- the pill -->
+				<div class="h-10 max-h-10 w-4 max-w-4 rounded-full bg-pink-500"></div>
+				<div class="h-fit flex-grow">
+					<h1 class="text-2xl font-semibold">Mechanical Condition</h1>
+					<p class="mt-3 text-gray-500">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi laudantium
+						iure, eligendi illo doloribus similique! Tenetur numquam reprehenderit
+						laborum dolores nam, itaque cumque error nemo sapiente iure saepe hic
+						repellendus!
+					</p>
+				</div>
+			</div>
+			<div class="flex items-start space-x-4">
+				<!-- the pill -->
+				<div class="h-10 max-h-10 w-4 max-w-4 rounded-full bg-yellow-500"></div>
+				<div class="h-fit flex-grow">
+					<h1 class="text-2xl font-semibold">Electrical</h1>
+					<p class="mt-3 text-gray-500">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi laudantium
+						iure, eligendi illo doloribus similique! Tenetur numquam reprehenderit
+						laborum dolores nam, itaque cumque error nemo sapiente iure saepe hic
+						repellendus!
+					</p>
+				</div>
+			</div>
+			<div class="flex items-start space-x-4">
+				<!-- the pill -->
+				<div class="h-10 max-h-10 w-4 max-w-4 rounded-full bg-red-500"></div>
+				<div class="h-fit flex-grow">
+					<h1 class="text-2xl font-semibold">Coachwork</h1>
+					<p class="mt-3 text-gray-500">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi laudantium
+						iure, eligendi illo doloribus similique! Tenetur numquam reprehenderit
+						laborum dolores nam, itaque cumque error nemo sapiente iure saepe hic
+						repellendus!
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-span-2 grid h-28 grid-cols-5 gap-x-10 rounded-lg">
+			<div
+				class="flex h-full items-center space-x-4 rounded-lg border-[.5px] px-8 shadow-md outline-none">
+				<div
+					class="flex size-16 items-center justify-center rounded-full bg-blue-500 text-white">
+					<WalletIcon classes="text-inherit" />
+				</div>
+				<div class="flex-grow">
+					<h1 class="text-lg font-semibold text-gray-600">Assessed Value</h1>
+					<span class="text-2xl font-semibold text-blue-700">1,700,000</span>
+				</div>
+			</div>
+			<div
+				class="flex h-full items-center space-x-4 rounded-lg border-[.5px] px-8 shadow-md outline-none">
+				<div
+					class="flex size-16 items-center justify-center rounded-full bg-yellow-500 text-white">
+					<WalletIcon classes="text-inherit" />
+				</div>
+				<div class="flex-grow">
+					<h1 class="text-lg font-semibold text-gray-600">Windscreen Value</h1>
+					<span class="text-2xl font-semibold text-blue-700">1,700,000</span>
+				</div>
+			</div>
+			<div
+				class="flex h-full items-center space-x-4 rounded-lg border-[.5px] px-8 shadow-md outline-none">
+				<div
+					class="flex size-16 items-center justify-center rounded-full bg-green-500 text-white">
+					<WalletIcon classes="text-inherit" />
+				</div>
+				<div class="flex-grow">
+					<h1 class="text-lg font-semibold text-gray-600">Market Value</h1>
+					<span class="text-2xl font-semibold text-blue-700">1,700,000</span>
+				</div>
+			</div>
+			<div
+				class="flex h-full items-center space-x-4 rounded-lg border-[.5px] px-8 shadow-md outline-none">
+				<div
+					class="flex size-16 items-center justify-center rounded-full bg-purple-500 text-white">
+					<WalletIcon classes="text-inherit" />
+				</div>
+				<div class="flex-grow">
+					<h1 class="text-lg font-semibold text-gray-600">Forced Sale Value</h1>
+					<span class="text-2xl font-semibold text-blue-700">1,700,000</span>
+				</div>
+			</div>
+			<div
+				class="flex h-full items-center space-x-4 rounded-lg border-[.5px] px-8 shadow-md outline-none">
+				<div
+					class="flex size-16 items-center justify-center rounded-full bg-red-500 text-white">
+					<WalletIcon classes="text-inherit" />
+				</div>
+				<div class="flex-grow">
+					<h1 class="text-lg font-semibold text-gray-600">Radio Value</h1>
+					<span class="text-2xl font-semibold text-blue-700">1,700,000</span>
+				</div>
+			</div>
+		</div>
+		<!-- assessment images -->
+		<div class="col-span-2 flex h-[50rem] space-x-5 outline-none">
+			<div class="flex h-full max-h-full w-[15%] flex-col space-y-3 overflow-y-scroll">
+				<img
+					v-for="a in 10"
+					:key="a"
+					src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=3939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt="Vehicle Image"
+					class="h-72 max-h-72 w-full rounded-sm" />
+			</div>
+			<div class="h-full max-h-full flex-grow">
+				<img
+					src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=3939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt="Vehicle Image"
+					class="size-full object-cover" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
