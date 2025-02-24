602e3fc27dea6ef65a60d029b242b2c4470f75c4
bikathi-martin <bikathimartin@outlook.com>
2025-02-24 10:13:00 +0300

    Simplify chatbot trigger form and homepage layout around chat section

--- components/forms/AIChatForm.vue
+++ components/forms/AIChatForm.vue
@@ -1,9 +1,11 @@
 <template>
-	<form @submit.prevent>
+	<form
+		@submit.prevent
+		class="w-full">
 		<div class="flex items-center space-x-3">
 			<input
 				type="text"
-				placeholder="Ask me anything..."
+				placeholder="Say hi or hello or konichiwa..."
 				class="w-3/4 rounded-full border-[1px] border-gray-600 px-5 py-4 text-gray-500 outline-none ring-1 ring-gray-600 transition-all duration-200 ease-in-out focus:border-gray-700 focus:ring-2 focus:ring-gray-700" />
 			<button
 				type="submit"
@@ -11,35 +13,6 @@
 				class="generic-form-submit mb-0 w-1/4 rounded-full shadow-md disabled:bg-blue-600">
 				Send Request
 			</button>
-		</div>
-
-		<!-- common questions section -->
-		<div>
-			<h1 class="my-10 text-center text-lg uppercase text-gray-500">Common Questions Are:</h1>
-			<div class="space-y-4">
-				<div class="grid grid-cols-2 space-x-4">
-					<button
-						class="ai-trigger-btns"
-						disabled>
-						I want to see all my valuation reports</button
-					><button
-						class="ai-trigger-btns"
-						disabled>
-						I'd like to view my ongoing service requests
-					</button>
-				</div>
-				<div class="grid grid-cols-2 space-x-4">
-					<button
-						class="ai-trigger-btns"
-						disabled>
-						I want to request for a vehicle valuation</button
-					><button
-						class="ai-trigger-btns"
-						disabled>
-						I need all accident reports from my account
-					</button>
-				</div>
-			</div>
 		</div>
 	</form>
 </template>
