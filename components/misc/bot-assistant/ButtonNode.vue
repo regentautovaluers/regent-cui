e0970ad8a9ab68b44d843428b293d55936c51215
bikathi-martin <bikathimartin@outlook.com>
2025-02-26 12:15:06 +0300

    Implement core node components required to interact with the bot i.e buttons and text node

---
+++ components/misc/bot-assistant/ButtonNode.vue
@@ -0,0 +1,16 @@
+<template>
+	<button
+		class="h-10 w-full whitespace-nowrap rounded-full border px-2 py-1 text-sm text-gray-500 outline-none ring-gray-500 hover:ring-2"
+		@click="emits('emit-click', props.value)">
+		{{ props.label }}
+	</button>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps({
+		label: { required: true, type: String },
+		value: { required: true, type: String },
+	});
+
+	const emits = defineEmits(['emit-click']);
+</script>
