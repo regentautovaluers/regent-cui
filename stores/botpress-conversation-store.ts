3b2333197c20b401a9616dcc3b57f23d1bb84c5b
bikathi-martin <bikathimartin@outlook.com>
2025-02-27 10:46:32 +0300

    Add more utility methods to botpress conversation store structure

--- stores/botpress-conversation-store.ts
+++ stores/botpress-conversation-store.ts
@@ -1,6 +1,8 @@
+import { type ButtonGroupNode, type TextNode } from '~/types';
+
 export const { state, getter, mutation, action, ...store } = createStore('', {
 	conversationId: '' as string,
-	conversation: [],
+	conversation: [] as any, // TODO: fix this
 });
 
 export const getConversationId = getter('getConversationId', (state) => state.conversationId);
@@ -8,3 +10,20 @@ export const getConversationId = getter('getConversationId', (state) => state.co
 export const setConversationId = mutation('setConversationId', (state, conversationId: string) => {
 	state.conversationId = conversationId;
 });
+
+export const cleanConversationId = mutation('cleanConversationId', (state) => {
+	state.conversationId = '';
+});
+
+export const setConversation = mutation(
+	'setConversation',
+	(state, conversation: ButtonGroupNode[] | TextNode) => {
+		state.conversation.push(conversation);
+	},
+);
+
+export const cleanConversation = mutation('cleanConversation', (state) => {
+	state.conversation = [];
+});
+
+export const getConversation = getter('getConversation', (state) => state.conversation);
