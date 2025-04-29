439b83f88d54cfe24b9fe145dbaa573775bc17e7
bikathi-martin <bikathimartin@outlook.com>
2025-04-29 11:28:46 +0300

    Fix for Ofetch to send the proper authn headers for different backends

--- plugins/ofetch.client.ts
+++ plugins/ofetch.client.ts
@@ -6,14 +6,27 @@ interface Headers {
 }
 
 export default defineNuxtPlugin(() => {
+	const { public: runtimeConfig } = useRuntimeConfig();
+	const { getAuthToken } = useAuth();
+
 	globalThis.$fetch = ofetch.create({
 		async onRequest({ options }) {
 			let headers: Headers = {};
-			headers['Ava-Basic-Auth'] = SecurityUtil.generateBase64Token(
-				'MOBIVALUER',
-				'LiV1tKgaqEtwPn7',
-			);
-			headers['Ava-Api-Key'] = 'fe08ab023b8f4d44a8612a64f4f642c9bcb34850';
+			console.log(options.baseURL);
+
+			// if the request is going to AVA
+			if (options.baseURL == runtimeConfig.AVA_BASE_URL) {
+				headers['Ava-Basic-Auth'] = SecurityUtil.generateBase64Token(
+					'MOBIVALUER',
+					'LiV1tKgaqEtwPn7',
+				);
+				headers['Ava-Api-Key'] = 'fe08ab023b8f4d44a8612a64f4f642c9bcb34850';
+			}
+
+			// if the request is going to valuation
+			if (options.baseURL == runtimeConfig.VALUATION_BASE_URL) {
+				headers['Authorization'] = `Bearer ${getAuthToken.value}`;
+			}
 
 			options.headers = { ...options.headers, ...headers };
 		},
