b39ccf25dd23f9d40921e71096c0f9855baa1ee6
bikathi-martin <bikathimartin@outlook.com>
2025-05-05 14:12:46 +0300

    Crucial fix for missing modal for creating corporate client

--- pages/console/vehicle-valuation/authorization-letter/create.vue
+++ pages/console/vehicle-valuation/authorization-letter/create.vue
@@ -4,12 +4,23 @@
 
 	<!-- Search Corp or Broker modal -->
 	<ParentModal
-		modal-id="search-corp-brokers"
+		modal-id="export-to-excel"
 		modal-title="Export to Excel"
 		class="h-[18rem]"
 		v-if="isExportExcelModalOpen"
 		@close-modal="isExportExcelModalOpen = false">
 		<ExportAuthorityLetter />
+	</ParentModal>
+
+	<!-- Search Corp or Broker modal -->
+	<ParentModal
+		modal-id="search-corp-brokers"
+		:modal-title="isPrincipalBroker() ? 'Search Corporate' : 'Search Brokers'"
+		class="h-[28rem]"
+		v-if="isSearchCorpBrokersModalOpen"
+		@close-modal="isSearchCorpBrokersModalOpen = false">
+		<SearchCorporateOrBroker
+			@close-search-agent-corp-modal="isSearchCorpBrokersModalOpen = false" />
 	</ParentModal>
 </template>
 
@@ -19,6 +30,7 @@
 		layout: 'console-layout',
 	});
 
+	const { isPrincipalBroker } = useAuth();
 	const isSearchCorpBrokersModalOpen: Ref<boolean> = ref(false);
 	const isExportExcelModalOpen: Ref<boolean> = ref(false);
 </script>
