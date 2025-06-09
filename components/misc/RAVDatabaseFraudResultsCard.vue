84771a247e6a97fa93b1dc832a2cd0c42a768c1c
bikathi-martin <bikathimartin@outlook.com>
2025-06-09 13:05:34 +0300

    Improve UI of results card

--- components/misc/RAVDatabaseFraudResultsCard.vue
+++ components/misc/RAVDatabaseFraudResultsCard.vue
@@ -1,11 +1,12 @@
 <template>
-	<div class="mt-4 h-fit min-h-32 rounded-md shadow-md bg-white p-8">
-		<h1 class="text-2xl font-semibold">
-			{{ props.regNo }} - {{ props.yearOfManufacture }} - {{ props.vehicleMake }}
+	<div class="mt-4 h-fit min-h-32 rounded-md border border-gray-100 bg-white p-8 shadow-md">
+		<h1 class="font-semibold">
+			{{ props.regNo }} - (Year Of Manufacture) {{ props.yearOfManufacture }} -
+			{{ props.vehicleMake }}
 			{{ props.vehicleModel }}
 		</h1>
 		<div class="my-2 flex h-12 items-center space-x-5 border-b border-gray-300">
-			<div class="flex w-fit items-center space-x-2 text-gray-600">
+			<div class="flex w-fit items-center space-x-2 text-sm text-gray-600">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -17,7 +18,7 @@
 				</svg>
 				<span>Chassis No: {{ props.chassisNumber }}</span>
 			</div>
-			<div class="flex w-fit items-center space-x-2 text-gray-600">
+			<div class="flex w-fit items-center space-x-2 text-sm text-gray-600">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -29,7 +30,7 @@
 				</svg>
 				<span class="text-gray-600">Engine No: {{ props.engineNumber }}</span>
 			</div>
-			<div class="flex w-fit items-center space-x-2 text-gray-600">
+			<div class="flex w-fit items-center space-x-2 text-sm text-gray-600">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -46,12 +47,13 @@
 				<span class="text-gray-600">Color: {{ props.vehicleColor }}</span>
 			</div>
 		</div>
-		<div class="grid min-h-20 grid-cols-2 divide-x divide-gray-300">
+		<div
+			class="tablet:grid-cols-2 tablet:divide-x tablet:divide-y-0 grid min-h-20 grid-cols-1 divide-y divide-gray-300">
 			<div class="min-h-full py-5">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="45"
-					height="45"
+					width="40"
+					height="40"
 					viewBox="0 0 16 16">
 					<path
 						fill="none"
@@ -60,7 +62,8 @@
 						stroke-width="1" />
 				</svg>
 				<h1 class="my-2 font-semibold">Defaulted Institution</h1>
-				<div class="mt-2 grid w-full grid-cols-2 gap-2 rounded-lg text-gray-600 xl:w-3/4">
+				<div
+					class="mt-2 grid w-full grid-cols-2 gap-2 rounded-lg text-sm text-gray-600 xl:w-3/4">
 					<div>
 						<h1 class="font-semibold">Name</h1>
 					</div>
@@ -92,15 +95,16 @@
 			<div class="p-5">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="45"
-					height="45"
+					width="40"
+					height="40"
 					viewBox="0 0 24 24">
 					<path
 						fill="currentColor"
 						d="M19.95 21q-3.125 0-6.187-1.35T8.2 15.8t-3.85-5.55T3 4.05V3h5.9l.925 5.025l-2.85 2.875q.55.975 1.225 1.85t1.45 1.625q.725.725 1.588 1.388T13.1 17l2.9-2.9l5 1.025V21z" />
 				</svg>
 				<h1 class="my-2 font-semibold">Case Contact Person</h1>
-				<div class="mt-2 grid w-full grid-cols-2 gap-2 rounded-lg text-gray-600 xl:w-3/4">
+				<div
+					class="mt-2 grid w-full grid-cols-2 gap-2 rounded-lg text-sm text-gray-600 xl:w-3/4">
 					<div>
 						<h1 class="font-semibold">Name</h1>
 					</div>
