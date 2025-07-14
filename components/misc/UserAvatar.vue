7937d6f1f3779ad14945cf4e0e8ef700fae4586d
bikathi-martin <bikathimartin@outlook.com>
2025-07-14 21:36:23 +0300

    Fix issue with sizing of user avatar

--- components/misc/UserAvatar.vue
+++ components/misc/UserAvatar.vue
@@ -1,19 +1,13 @@
 <template>
 	<div
 		:class="[
-			'flex items-center justify-center rounded-full border bg-blue-600 outline-none',
-			props.circleSize,
+			'ax-w-12 flex max-h-12 min-h-12 min-w-12 items-center justify-center rounded-full border bg-blue-600 outline-none',
 		]">
-		<span :class="['font-semibold text-slate-100', props.initialsSize]">{{ firstLetter }}</span>
+		<span :class="['text-xl font-semibold text-slate-100']">{{ firstLetter }}</span>
 	</div>
 </template>
 
 <script setup lang="ts">
-	const props = defineProps({
-		circleSize: { required: false, type: String, default: 'size-11' },
-		initialsSize: { required: false, type: String, default: 'text-lg' },
-	});
-
 	const { getPrincipal } = useAuth();
 	const firstLetter: ComputedRef<string> = computed(() =>
 		getPrincipal.value.username.substring(0, 1),
