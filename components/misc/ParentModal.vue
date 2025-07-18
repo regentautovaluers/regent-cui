c35593f70ed17a188d58e64127af1efea54a8a2c
bikathi-martin <bikathimartin@outlook.com>
2025-07-18 06:06:17 +0300

    Add a lightly darkened modal bg color

--- components/misc/ParentModal.vue
+++ components/misc/ParentModal.vue
@@ -4,10 +4,10 @@
 		:id="props.modalId"
 		tabindex="-1"
 		:data-modal-placement="props.modalPlacement"
-		class="fixed top-0 right-0 left-0 z-50 hidden h-[100%] max-h-full w-full overflow-x-hidden overflow-y-auto bg-white/10 p-4 md:inset-0">
+		class="fixed top-0 right-0 left-0 z-50 hidden h-[100%] max-h-full w-full overflow-x-hidden overflow-y-auto bg-[#242424]/30 md:inset-0">
 		<div class="relative max-h-full w-full max-w-2xl">
 			<!-- Modal content -->
-			<div class="relative rounded-xl bg-white shadow-sm">
+			<div class="relative m-2 rounded-xl bg-white shadow-sm">
 				<!-- Modal header -->
 				<div class="flex items-center justify-between rounded-t-xl bg-blue-50/50 px-5 py-3">
 					<h3 class="font-semibold text-gray-500">{{ props.modalTitle }}</h3>
