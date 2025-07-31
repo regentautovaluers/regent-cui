b1c3a80e297bba1d6749d1a17c2a6675e283ae03
bikathi-martin <bikathimartin@outlook.com>
2025-07-31 11:12:55 +0300

    Improve UI of tabs listing, switcher and selector

--- pages/console/settings.vue
+++ pages/console/settings.vue
@@ -2,44 +2,42 @@
 	<div class="console-layout-spacing">
 		<div class="mb-5 flex h-fit flex-col">
 			<div
-				class="laptop:w-fit flex w-full items-center space-x-2 rounded-lg border-[1px] bg-white">
+				class="laptop:w-fit hide-scrollbar mb-5 flex w-full items-center space-x-2 overflow-x-scroll rounded-full border-[1px] bg-white p-1">
 				<NuxtLink
 					:to="{ name: 'settings-my-account' }"
-					class="rounded-lg p-2 text-sm text-gray-500"
+					class="h-10 rounded-full p-2 text-sm whitespace-nowrap text-gray-500"
 					:class="
 						doesRouteNameMatch('settings-my-account')
-							? 'bg-gray-200 font-semibold'
-							: 'hover:bg-gray-200'
+							? 'bg-gray-200'
+							: 'hover:bg-gray-100'
 					"
 					>My Account</NuxtLink
 				>
 				<NuxtLink
 					:to="{ name: 'vehicle-valuation-manage-user' }"
-					class="rounded-lg p-2 text-sm text-gray-500"
+					class="h-10 rounded-full p-2 text-sm whitespace-nowrap text-gray-500"
 					:class="
 						doesRouteNameMatch('vehicle-valuation-manage-user')
-							? 'bg-gray-200 font-semibold'
-							: 'hover:bg-gray-200'
+							? 'bg-gray-200'
+							: 'hover:bg-gray-100'
 					"
 					>Manage Users</NuxtLink
 				>
 				<NuxtLink
 					:to="{ name: 'vehicle-valuation-add-user' }"
-					class="rounded-lg p-2 text-sm text-gray-500"
+					class="h-10 rounded-full p-2 text-sm whitespace-nowrap text-gray-500"
 					:class="
 						doesRouteNameMatch('vehicle-valuation-add-user')
-							? 'bg-gray-200 font-semibold'
-							: 'hover:bg-gray-200'
+							? 'bg-gray-200'
+							: 'hover:bg-gray-100'
 					"
 					>Add New User</NuxtLink
 				>
 				<NuxtLink
 					:to="{ name: 'corp-branches' }"
-					class="rounded-lg p-2 text-sm text-gray-500"
+					class="h-10 rounded-full p-2 text-sm whitespace-nowrap text-gray-500"
 					:class="
-						doesRouteNameMatch('corp-branches')
-							? 'bg-gray-200 font-semibold'
-							: 'hover:bg-gray-200'
+						doesRouteNameMatch('corp-branches') ? 'bg-gray-200' : 'hover:bg-gray-100'
 					"
 					>Corporate Branches</NuxtLink
 				>
