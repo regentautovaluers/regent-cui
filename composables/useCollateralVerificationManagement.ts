e54795eb6683977b0a7159d9e066680a39e1f189
bikathi-martin <bikathimartin@outlook.com>
2025-08-14 13:04:01 +0300

    Add composable and UI to load corporate's invoices

--- composables/useCollateralVerificationManagement.ts
+++ composables/useCollateralVerificationManagement.ts
@@ -73,6 +73,98 @@ function useCollateralVerificationTokensManagement() {
 	};
 }
 
+function useCollateralVerificationInvoiceManagement() {
+	const runtimeConfig = useRuntimeConfig();
+	const { getPrincipal } = useAuth();
+	const collateralVerificationInvoices: ComputedRef<any[]> = computed(
+		() => collateralVerificationInvoiceList.value?.invoices,
+	);
+	const totalPages: ComputedRef<number> = computed(
+		() => collateralVerificationInvoiceList.value?.totalPages,
+	);
+	const page: Ref<number> = ref(0);
+	const pageSize: number = 10;
+	const paymentStatus: Ref<'paid' | 'unpaid' | 'partial' | 'stale' | 'void' | null> = ref(null);
+	const startDate: Ref<string | null> = ref(null);
+	const endDate: Ref<string | null> = ref(null);
+
+	const {
+		status: fetchCollateralVerificationInvoiceListStatus,
+		execute: executeFetchCollateralVerificationInvoiceList,
+		error: fetchCollateralVerificationInvoiceListError,
+		data: collateralVerificationInvoiceList,
+	} = useFetch(
+		() => {
+			let requestURL = `/api/v1/invoices/m8cyni35?page=${page.value}&size=${pageSize}`;
+
+			if (startDate.value !== null) {
+				requestURL = requestURL + `&startDate=${startDate.value}`;
+			}
+
+			if (endDate.value !== null) {
+				requestURL = requestURL + `&endDate=${endDate.value}`;
+			}
+
+			if (paymentStatus.value !== null) {
+				requestURL = requestURL + `&paymentStatus=${paymentStatus.value}`;
+			}
+
+			return requestURL;
+		},
+		{
+			key: 'collateral-verifications-invoices',
+			baseURL: runtimeConfig.public.FRAUD_DETECTION_BASE_URL,
+			method: 'GET',
+			server: false,
+			lazy: true,
+			transform(response: any) {
+				if (
+					response?.data &&
+					Array.isArray(response.data) &&
+					(response.data as []).length > 0
+				) {
+					return {
+						invoices: response.data,
+						totalPages: response.pagination?.pages || 0,
+					};
+				} else {
+					return {
+						valuations: [],
+						totalPages: 0,
+					};
+				}
+			},
+			watch: [page],
+		},
+	);
+
+	function clearFilters(): void {
+		startDate.value = null;
+		endDate.value = null;
+		paymentStatus.value = null;
+
+		// ecxecute the request
+		executeFetchCollateralVerificationInvoiceList();
+	}
+
+	return {
+		page,
+		paymentStatus,
+		startDate,
+		endDate,
+		totalPages,
+		collateralVerificationInvoices,
+		fetchCollateralVerificationInvoiceListStatus,
+		fetchCollateralVerificationInvoiceListError,
+		executeFetchCollateralVerificationInvoiceList,
+		clearFilters,
+	};
+}
+
 function useCollateralVerificationsHistoryManagement() {}
 
-export { useCollateralVerificationTokensManagement, useCollateralVerificationsHistoryManagement };
+export {
+	useCollateralVerificationTokensManagement,
+	useCollateralVerificationsHistoryManagement,
+	useCollateralVerificationInvoiceManagement,
+};
---
+++ pages/console/collateral-verification/management/invoices.vue
@@ -0,0 +1,319 @@
+<template>
+	<div class="flex flex-col">
+		<form
+			@submit.prevent="executeFetchCollateralVerificationInvoiceList()"
+			class="table-filters-form">
+			<button
+				data-popover-target="payment-status-pop-over"
+				data-popover-trigger="click"
+				data-popover-placement="bottom"
+				type="button"
+				:class="[
+					'table-filter-form-options',
+					paymentStatus ? 'bg-blue-600 text-white hover:bg-blue-600' : '',
+				]">
+				Payment Status
+			</button>
+
+			<div
+				data-popover
+				id="payment-status-pop-over"
+				role="tooltip"
+				class="invisible absolute z-10 inline-block w-64 rounded-lg border border-gray-200 bg-white text-sm text-gray-500 opacity-0 shadow-xs transition-opacity duration-300">
+				<div class="space-y-2 px-3 py-2">
+					<div
+						class="flex h-10 items-center rounded-lg border border-gray-200 ps-4 hover:bg-gray-200"
+						v-for="(ps, index) in ['paid', 'unpaid', 'partial', 'stale', 'void']"
+						:key="index">
+						<input
+							id="has-spare-tyre"
+							type="radio"
+							:value="ps"
+							v-model="paymentStatus"
+							name="bordered-radio"
+							class="size-5 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500" />
+						<label
+							for="has-spare-tyre"
+							class="ms-2 w-full py-4 font-medium text-gray-600"
+							>{{ ps }}</label
+						>
+					</div>
+				</div>
+				<div data-popper-arrow></div>
+			</div>
+
+			<button
+				data-popover-target="date-range-pop-over"
+				data-popover-trigger="click"
+				data-popover-placement="bottom"
+				type="button"
+				:class="[
+					'table-filter-form-options',
+					startDate || endDate ? 'bg-blue-600 text-white hover:bg-blue-600' : '',
+				]">
+				Date Range
+			</button>
+
+			<div
+				data-popover
+				id="date-range-pop-over"
+				role="tooltip"
+				class="invisible absolute z-10 inline-block w-64 rounded-lg border border-gray-200 bg-white text-sm text-gray-500 opacity-0 shadow-xs transition-opacity duration-300">
+				<div class="space-y-2 px-3 py-2">
+					<!-- Starting date Field -->
+					<div>
+						<label
+							for="cover-period-starts"
+							class="generic-input-label mb-0 text-xs"
+							>Start Date</label
+						>
+						<input
+							type="date"
+							id="cover-period-starts"
+							name="cover-period-starts"
+							class="generic-input h-10"
+							placeholder="Select"
+							pattern="\d{4}-\d{2}-\d{2}"
+							required
+							v-model="startDate" />
+					</div>
+
+					<!-- Ending date Field -->
+					<div>
+						<label
+							for="cover-period-starts"
+							class="generic-input-label mb-0 text-xs"
+							>End Date</label
+						>
+						<input
+							type="date"
+							id="cover-period-starts"
+							class="generic-input h-10"
+							placeholder="Select"
+							pattern="\d{4}-\d{2}-\d{2}"
+							required
+							v-model="endDate" />
+					</div>
+				</div>
+				<div data-popper-arrow></div>
+			</div>
+			<span
+				class="text-lg"
+				v-if="startDate || endDate || paymentStatus"
+				>&middot;</span
+			>
+			<button
+				type="submit"
+				class="table-filter-form-options flex items-center space-x-2"
+				v-if="startDate || endDate || paymentStatus">
+				<span class="material-symbols--error-rounded size-5"></span>
+				<span>Apply Filters</span>
+			</button>
+			<button
+				type="button"
+				@click="clearFilters()"
+				class="table-filter-form-options flex items-center space-x-2"
+				v-if="startDate || endDate || paymentStatus">
+				<span class="material-symbols-light--close-rounded size-5"></span>
+				<span>Clear Filters</span>
+			</button>
+		</form>
+
+		<div class="">
+			<!-- div to show when there is a fetch error -->
+			<div
+				v-if="fetchCollateralVerificationInvoiceListStatus === 'error'"
+				class="mt-5 flex h-full flex-col items-center justify-center space-y-4 rounded-lg border shadow-sm">
+				<BirdieNotFoundIcon />
+				<h1 class="font-semibold text-gray-500">Failed to load data!</h1>
+				<button
+					class="inline-flex items-center space-x-2 rounded-lg border bg-transparent px-2 py-1 text-gray-500 hover:text-gray-600"
+					@click="refreshPage">
+					<span>Refresh</span>
+					<RefreshIcon classes="size-6" />
+				</button>
+			</div>
+
+			<!-- div to show when there are no authorization letters -->
+			<div
+				class="mt-5 flex h-full flex-col items-center justify-center space-y-4 rounded-lg border shadow-sm"
+				v-else-if="
+					fetchCollateralVerificationInvoiceListStatus === 'success' &&
+					!collateralVerificationInvoices.length
+				">
+				<BirdieNotFoundIcon />
+				<h1 class="font-semibold text-gray-500">
+					Oops! Seems like you have no valuations!
+				</h1>
+			</div>
+
+			<!-- div to show when there are authority letters -->
+			<div
+				class="flex h-full flex-col"
+				v-else>
+				<div class="mt-5 flex-grow">
+					<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+						<table class="w-full text-left text-gray-500">
+							<thead class="bg-gray-100 text-sm text-gray-700 uppercase">
+								<tr>
+									<!-- Details will show client name and their contact -->
+
+									<th
+										scope="col"
+										class="table-headers tablet:table-cell hidden">
+										Invoice Number
+									</th>
+									<th
+										scope="col"
+										class="table-headers tablet:table-cell hidden">
+										Time Period
+									</th>
+									<th
+										scope="col"
+										class="table-headers">
+										Invoice (Ksh)
+									</th>
+									<th
+										scope="col"
+										class="table-headers desktop-4k:table-cell hidden">
+										Paid (Ksh)
+									</th>
+									<th
+										scope="col"
+										class="table-headers laptop:table-cell hidden">
+										Last Payment At
+									</th>
+									<th
+										scope="col"
+										class="table-headers laptop:table-cell hidden">
+										Fully Paid At
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<!-- loading state -->
+								<tr
+									class="border-b bg-white hover:bg-gray-100"
+									v-if="
+										fetchCollateralVerificationInvoiceListStatus === 'pending'
+									"
+									v-for="a in 10"
+									:key="a">
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>regenthq</span
+										>
+									</td>
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>domain role</span
+										>
+									</td>
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>agency</span
+										>
+									</td>
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>authorizedby</span
+										>
+									</td>
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>bookingstage</span
+										>
+									</td>
+									<td class="p-6 text-gray-300">
+										<span class="animate-pulse rounded-lg bg-gray-300"
+											>bookingstage</span
+										>
+									</td>
+									<td></td>
+								</tr>
+
+								<!-- the actual data -->
+								<tr
+									class="border-b bg-white text-sm hover:bg-gray-100"
+									v-else
+									v-for="(invoice, index) in collateralVerificationInvoices"
+									:key="index">
+									<td
+										class="laptop:table-cell hidden w-72 max-w-72 p-3 text-sm text-wrap font-semibold text-blue-500">
+										{{ invoice.invoiceNumber }}
+									</td>
+									<td class="tablet:table-cell hidden p-3">
+										<span class="w-fit">
+											from
+											{{
+												(invoice.periodStart as string)?.split('T')[0]
+											}}</span
+										>
+										<br />
+										<span class="w-fit rounded-lg bg-gray-200 px-1"
+											>to
+											{{ (invoice.periodEnd as string)?.split('T')[0] }}</span
+										>
+									</td>
+									<td class="laptop:table-cell hidden p-3 text-center text-sm font-semibold text-pink-500">
+										{{ invoice.amount }}
+									</td>
+									<td class="laptop:table-cell hidden p-3 text-center text-sm font-semibold text-pink-500">
+										{{ invoice.amountPaid }}
+									</td>
+									<td class="laptop:table-cell hidden p-3 text-sm ">
+										{{ (invoice.lastPaymentAt as string)?.split('T')[0] }}
+									</td>
+									<td class="laptop:table-cell hidden p-3 text-sm">
+										{{ (invoice.paidAt as string)?.split('T')[0] }}
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+				<!-- page controls -->
+				<div class="mt-8 flex h-12 min-h-12 items-center justify-between">
+					<h1 class="text-sm font-semibold text-gray-500">
+						Showing {{ page + 1 }} of {{ totalPages }} pages.
+					</h1>
+					<div class="h-full space-x-2 md:space-x-4">
+						<button
+							class="table-page-buttons"
+							@click="page -= 1"
+							:disabled="page === 0 || totalPages == 1">
+							Previous
+						</button>
+						<button
+							class="table-page-buttons"
+							@click="page += 1"
+							:disabled="page === totalPages - 1 || totalPages == 1">
+							Next
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		name: 'collateral-verification-view-invoices',
+		layout: 'console-layout',
+	});
+
+	const {
+		page,
+		paymentStatus,
+		startDate,
+		endDate,
+		totalPages,
+		collateralVerificationInvoices,
+		fetchCollateralVerificationInvoiceListStatus,
+		fetchCollateralVerificationInvoiceListError,
+		executeFetchCollateralVerificationInvoiceList,
+		clearFilters,
+	} = useCollateralVerificationInvoiceManagement();
+</script>
