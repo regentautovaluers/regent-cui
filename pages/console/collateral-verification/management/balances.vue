2219ad13e3a488ade6660acfbc3e25f0f12920ac
bikathi-martin <bikathimartin@outlook.com>
2025-08-14 13:06:19 +0300

    Minor UI and code cleanup

--- pages/console/collateral-verification/management/balances.vue
+++ pages/console/collateral-verification/management/balances.vue
@@ -4,12 +4,14 @@
 			class="laptop:flex-row laptop:space-y-0 laptop:space-x-4 mb-4 flex h-52 flex-col space-y-4">
 			<div
 				class="laptop:w-1/2 flex w-full flex-col items-center justify-center space-y-4 rounded-md border-[1px] p-4 text-gray-500 shadow-sm ring-gray-400 transition-all duration-150 outline-none hover:ring-2">
-				<span class="text-7xl">{{ getCollateralVerificationTokenInfo?.balance }}</span
+				<span class="text-7xl">{{
+					getCollateralVerificationTokenInfo?.balance ?? 'N/A'
+				}}</span
 				><span class="text-sm">Tokens Left</span>
 			</div>
 			<div
 				class="laptop:w-1/2 flex w-full flex-col items-center justify-center rounded-md border-[1px] p-4 shadow-sm ring-gray-400 transition-all duration-150 outline-none hover:ring-2">
-				hello world 2
+				top up
 			</div>
 		</div>
 		<div></div>
@@ -22,6 +24,5 @@
 		layout: 'console-layout',
 	});
 
-	const { fetchCollateralVerificationTokensStatus, getCollateralVerificationTokenInfo } =
-		useCollateralVerificationTokensManagement();
+	const { getCollateralVerificationTokenInfo } = useCollateralVerificationTokensManagement();
 </script>
