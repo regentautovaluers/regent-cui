158be6893665d3c525c3361c37eb7595dd15bf2b
bikathi-martin <bikathimartin@outlook.com>
2025-08-19 10:43:04 +0300

    Add bar chart to represent how IPRS and collateral checks are distributed

---
+++ components/charts/CollateralVerificationSearchesBarChart.vue
@@ -0,0 +1,44 @@
+<template>
+	<Bar
+		:data="data"
+		:options="{
+			responsive: true,
+			maintainAspectRatio: false,
+			scales: {
+				y: {
+					beginAtZero: true,
+				},
+			},
+			plugins: {
+				legend: { display: false },
+			},
+		}" />
+</template>
+
+<script setup lang="ts">
+	import { Bar } from 'vue-chartjs';
+	const props = defineProps({
+		labels: { type: Array<string>, required: true },
+		data: { type: Array<number>, required: true },
+	});
+
+	const data = computed(() => {
+		return {
+			labels: props.labels,
+			datasets: [
+				{
+					// label: 'Data One',
+					backgroundColor: '#2563EB',
+					data: props.data,
+					borderRadius: 4,
+					borderWidth: 1,
+				},
+			],
+		};
+	});
+
+	const options = {
+		responsive: true,
+		maintainAspectRatio: false,
+	};
+</script>
