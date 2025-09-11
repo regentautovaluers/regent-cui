2741154e5c3db0ec610db7533e2336a4d74d4164
bikathi-martin <bikathimartin@outlook.com>
2025-09-11 13:31:35 +0300

    Improve API typing

--- server/api/regent-tracking/load-vehicles.get.ts
+++ server/api/regent-tracking/load-vehicles.get.ts
@@ -3,13 +3,15 @@ import { TrackedVehicles } from '~/types/regent-tracking/tracked-vehicles';
 
 export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig();
-	const api_hash = getRouterParam(event, 'api_hash');
+	const query = getQuery(event);
 
 	// Set source context for response wrapper
 	event.context.source = 'regent-tracking-service';
 
-	const vehicleData: any = await makeProxyRequest(
-		`${config.public.REGENT_TRACK_BASE_URL}/api/get_devices?lang=en&user_api_hash=${`$2y$10$VG5xofVqWW6B1gu2zLDFYezsoLkyUonucCKZyR5tAFqb7XV5Tx2yi`}`,
+	const vehicleData = await makeProxyRequest<
+		{ id: number; title: string; items: TrackedVehicles[] }[]
+	>(
+		`${config.public.REGENT_TRACK_BASE_URL}/api/get_devices?lang=en&user_api_hash=${query.api_hash}`,
 	);
 
 	// Simple transformation
