67cc44f478231c1d335c8628b311748a25c70f52
bikathi-martin <bikathimartin@outlook.com>
2025-09-22 15:27:25 +0300

    Temporarily disable dev port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	devServer: {
-		port: 8000,
-	},
+	// devServer: {
+	// 	port: 8000,
+	// },
 	css: ['~/assets/css/main.css'],
 	vite: {
 		plugins: [tailwindcss()],
