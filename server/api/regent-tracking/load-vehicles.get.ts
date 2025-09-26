bdc260f35055554ca9f4fefcf35718717636659d
bikathi-martin <bikathimartin@outlook.com>
2025-09-26 12:27:35 +0300

    Add pagination query params to request

--- server/api/regent-tracking/load-vehicles.get.ts
+++ server/api/regent-tracking/load-vehicles.get.ts
@@ -4,13 +4,21 @@ import { TrackedVehicles } from '~/types/regent-tracking/tracked-vehicles';
 export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig();
 	const query = getQuery(event);
+	let endpoint = `${config.REGENT_TRACK_BASE_URL}/api/get_devices?lang=en&user_api_hash=${query.api_hash}`;
+
+	if (query.page) {
+		endpoint = endpoint + `&page=${query.page}`;
+	}
+
+	if (query.limit) {
+		endpoint = endpoint + `&limit=${query.limit}`;
+	}
 
 	try {
-		const vehicleData = await makeProxyRequest<
-			{ id: number; title: string; items: TrackedVehicles[] }[]
-		>(
-			`${config.REGENT_TRACK_BASE_URL}/api/get_devices?lang=en&user_api_hash=${query.api_hash}`,
-		);
+		const vehicleData =
+			await makeProxyRequest<{ id: number; title: string; items: TrackedVehicles[] }[]>(
+				endpoint,
+			);
 		return sendSuccessResponse(
 			event,
 			vehicleData.flatMap((vehicle) => vehicle.items as TrackedVehicles[]),
