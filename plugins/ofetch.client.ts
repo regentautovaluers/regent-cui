2114fb81cf709073117cb2bc01f5c23ef9e3a826
bikathi <bikathimartin@aol.com>
2026-06-09 19:45:23 +0300

    (Fix) - Fix critical bug with adding headers to direct AVA requests

--- plugins/ofetch.client.ts
+++ plugins/ofetch.client.ts
@@ -1,72 +1,27 @@
 import { ofetch } from 'ofetch';
 
 export default defineNuxtPlugin(() => {
-	const {
-		AVA_BASE_URL,
-		LEGACY_VALUATION_BASE_URL,
-		VALUATION_BASE_URL,
-		LEGACY_VALUATION_API_KEY,
-	} = useRuntimeConfig();
+	const config = useRuntimeConfig();
 
 	globalThis.$fetch = ofetch.create({
 		async onRequest({ options }) {
 			const baseURL = options.baseURL;
 			let headers: Record<string, string> = {};
 
-			if (baseURL == VALUATION_BASE_URL) {
+			if (baseURL == config.public.VALUATION_BASE_URL) {
 				const valuationAuthToken = useCookie('valuation_auth_token');
 				headers['Authorization'] = `Bearer ${valuationAuthToken.value}`;
 			}
 
-			if (baseURL == AVA_BASE_URL) {
+			if (baseURL == config.public.AVA_BASE_URL) {
 				const avaBasicAuthToken = useCookie('ava_basic_auth_token');
-				const avaApiKey = useCookie('ava_basic_auth_token');
+				const avaApiKey = useCookie('ava_api_key');
 
 				headers['Ava-Basic-Auth'] = avaBasicAuthToken.value as string;
 				headers['Ava-Api-Key'] = avaApiKey.value as string;
 			}
 
-			if (baseURL == LEGACY_VALUATION_BASE_URL) {
-				headers['X-API-Key'] = LEGACY_VALUATION_API_KEY;
-			}
-
 			options.headers = { ...options.headers, ...headers };
-
-			/*
-			let headers: Headers = {};
-
-			// if the request is going to AVA
-			if (options.baseURL == runtimeConfig.AVA_BASE_URL) {
-				headers['Ava-Basic-Auth'] = getAvaBasicAuthToken.value!;
-				headers['Ava-Api-Key'] = getAvaApiKey.value!;
-			}
-
-			// if the request us going to Fraud Detection
-			if (options.baseURL == runtimeConfig.FRAUD_DETECTION_BASE_URL) {
-				headers['X-Client-Id'] = encryptCorporateClientId(
-					getPrincipal()?.corpOrganization.corpId!,
-				);
-
-				// API-key
-				const USERNAME = getPrincipal()?.corpOrganization.corpId!;
-				const PASSWORD = 'k8#F$j2!L9@qW7%zX5^pR3&vN6*';
-				headers['X-Api-Key'] = SecurityUtil.generateApiKey(USERNAME, PASSWORD);
-
-				// if we are paying up for tokens -> not via invoice
-				if (route.name == 'collateral-verification-home') {
-					headers['Entity-Token'] = encryptCorporateClientId(
-						getPrincipal()?.corpOrganization.corpId!,
-					);
-				}
-			}
-
-			// if the request is going to valuation
-			if (options.baseURL == runtimeConfig.VALUATION_BASE_URL) {
-				headers['Authorization'] = `Bearer ${getValuationAuthToken.value}`;
-			}
-
-			options.headers = { ...options.headers, ...headers };
-			*/
 		},
 	});
 });
