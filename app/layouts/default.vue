2b45dfc2cd3f29fd8bab33420bc84f05eda16a4e
bikathi <bikathimartin@aol.com>
2026-09-17 10:06:10 +0300

    Fix height growth issues

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,11 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="h-screen flex">
+  <div class="h-screen w-screen min-h-screen flex">
     <AsideNavigation></AsideNavigation>
     <div class="flex-1 min-h-0 overflow-y-auto relative">
       <Header></Header>
-      <main class="p-5 tablet:p-10 laptop:p-15 laptop-lg:p-25 lg:pt-40 min-h-screen h-screen">
+      <main class="p-5 laptop:p-15 laptop-lg:p-25 lg:pt-40 min-h-full">
         <slot></slot>
       </main>
     </div>
